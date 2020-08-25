6d0916a2c1ada92f79162b14644213f4578f3e13
Patel <pateld6@pllimskhpc1.mskcc.org>
2020-08-25 10:52:04 -0400

    typo changed

--- test/test_feature_table.py
+++ test/test_feature_table.py
@@ -28,8 +28,8 @@ def test_local_feature_table(runner):
     feature_table_path = os.path.join(BASE_DIR, "features/feature_table")
     
     # Read Delta Table and Verify 
-    feature_table = DeltaTable.forPath(SPARK, feature_table_path)
-    spark.read.format("delta").load(feature_table_path)
+    # feature_table = DeltaTable.forPath(spark, feature_table_path)
+    feature_table = spark.read.format("delta").load(feature_table_path)
     assert feature_table.toDF().count() == 15
     print ("Test Local feature table passed.")
 
