e1d609ff58a249b7e6f4c30057b99aca8c9d53c6
Patel <pateld6@pllimskhpc1.mskcc.org>
2020-08-28 12:09:21 -0400

    cleanup tests, switch to spark read/load df instead of Delta FromPath

--- test/test_preprocess_feature_integration.py
+++ test/test_preprocess_feature_integration.py
@@ -30,115 +30,29 @@ def cleanup():
     if os.path.exists(feature_dir):
         shutil.rmtree(feature_dir)
 
-def test_local_feature_table(spark_session):
+def test_local_feature_table_generation(spark_session):
     from delta.tables import DeltaTable
     
     # Need to add python dependencies to the spark context, otherwise get module not found error
     spark_session.sparkContext.addPyFile("src/dl_processing/preprocess_feature.py")
     spark_session.sparkContext.addPyFile("src/sparksession.py")
-
 
     # Test no query, default naming
     # Build Feature Table
-    generate_feature_table(BASE_DIR, TARGET_SPACING, spark_session, False, "", "feature-table")
+    generate_feature_table(BASE_DIR, TARGET_SPACING, spark_session, False, "subtype='BRCA1' or subtype='BRCA2'", "feature-table-test-name")
 
     # read and verify correct feature table generated
-    feature_table_path = os.path.join(BASE_DIR, "features/feature-table")
+    feature_table_path = os.path.join(BASE_DIR, "features/feature-table-test-name")
     
     # Read Delta Table and Verify 
-    feature_table = DeltaTable.forPath(spark_session, feature_table_path)
-    feature_df = feature_table.toDF()
-    assert feature_table.toDF().count() == 15
+    feature_df = spark_session.read.format("delta").load(feature_table_path)
+    assert feature_df.count() == 11
     print ("test_local_feature_table passed.")
 
-def test_local_feature_table_query(spark_session):
-    from delta.tables import DeltaTable
-    
-    # Need to add python dependencies to the spark context, otherwise get module not found error
-    spark_session.sparkContext.addPyFile("src/dl_processing/preprocess_feature.py")
-    spark_session.sparkContext.addPyFile("src/sparksession.py")
-
-
-    # Test 2: SQL query test 
-    # Build Feature Table 
-    generate_feature_table(BASE_DIR, TARGET_SPACING, spark_session, False, "subtype='BRCA1' or subtype='BRCA2'", "feature-table")
-
-    # read and verify correct feature table generated
-    feature_table_path = os.path.join(BASE_DIR, "features/feature-table")
-    
-    # Read Delta Table and Verify 
-    feature_table = DeltaTable.forPath(spark_session, feature_table_path)
-    feature_df = feature_table.toDF()
-    assert feature_table.toDF().count() == 11
-    print ("test_local_feature_table_query passed.")
-
-def test_local_feature_table_name(spark_session):
-    from delta.tables import DeltaTable
-    
-    # Need to add python dependencies to the spark context, otherwise get module not found error
-    spark_session.sparkContext.addPyFile("src/dl_processing/preprocess_feature.py")
-    spark_session.sparkContext.addPyFile("src/sparksession.py")
-
-    # Test 3: test feature table naming 
-    # Build Feature Table
-    generate_feature_table(BASE_DIR, TARGET_SPACING, spark_session, False, "", "test-name")
-
-    # read and verify correct feature table generated
-    feature_table_path = os.path.join(BASE_DIR, "features/test-name")
-    
-    # Read Delta Table and Verify 
-    feature_table = DeltaTable.forPath(spark_session, feature_table_path)
-    feature_df = feature_table.toDF()
-    assert feature_table.toDF().count() == 15
-    print ("test_local_feature_table_name passed.")
-
-def test_local_feature_table_name_and_query(spark_session):
-    from delta.tables import DeltaTable
-    
-    # Need to add python dependencies to the spark context, otherwise get module not found error
-    spark_session.sparkContext.addPyFile("src/dl_processing/preprocess_feature.py")
-    spark_session.sparkContext.addPyFile("src/sparksession.py")
-
-    # Test 4: test feature table naming and filtering 
-    # Build Feature Table
-    generate_feature_table(BASE_DIR, TARGET_SPACING, spark_session, False, "", "test-name-two")
-
-    # read and verify correct feature table generated
-    feature_table_path = os.path.join(BASE_DIR, "features/test-name-two")
-    
-    # Read Delta Table and Verify 
-    feature_table = DeltaTable.forPath(spark_session, feature_table_path)
-    feature_df = feature_table.toDF()
-    assert feature_table.toDF().count() == 15
-    print ("test_local_feature_table_name_and_query passed.")
-
-
 def test_feature_table_cli():
-    
-    # Test 1, no query or naming
-    result = runner.invoke(cli, "--spark_master_uri local[*] --base_directory {} --target_spacing 1 1 3".format(BASE_DIR))
-    assert result.exit_code == 0
-    feature_dir = os.path.join(BASE_DIR, "features")
-    if os.path.exists(feature_dir):
-        shutil.rmtree(feature_dir)
-    print("CLI Test 1 passed.")
-
-    # Test 2, query 
-    result = runner.invoke(cli, "--spark_master_uri local[*] --base_directory {} --target_spacing 1 1 3 --query \"annotation_uid=\'annotation-1\'\"".format(BASE_DIR))
-    assert result.exit_code == 0
-    print("CLI Test 2 passed.")
-
-    # Test 3, name
-    result = runner.invoke(cli, "--spark_master_uri local[*] --base_directory {} --target_spacing 1 1 3 --feature_table_output_name test-cli-1".format(BASE_DIR))
-    assert result.exit_code == 0
-    print("CLI Test 3 passed.")
-
-    # Test 4, query and name
     result = runner.invoke(cli, "--spark_master_uri local[*] --base_directory {} --target_spacing 1 1 3 --query \"subtype=\'CCNE1\'\" --feature_table_output_name test-cli-2".format(BASE_DIR))
     assert result.exit_code == 0
-    print("CLI Test 4 passed.")
-
-    print("All CLI tests passed.")
+    print("CLI test passed.")
 
 
 def test_feature_table_cli_missing_params():
