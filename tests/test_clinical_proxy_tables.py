63d20ad677fddd64226634d62479e9f32d4c6be7
arfath pasha <arfathpasha@gmail.com>
2020-09-14 09:32:39 -0400

    add clinical proxy tests

---
+++ tests/test_clinical_proxy_tables.py
@@ -0,0 +1,51 @@
+'''
+Created on September 11, 2020
+
+@author: pashaa@mskcc.org
+'''
+import os, shutil, sys
+import pytest
+from click.testing import CliRunner
+
+sys.path.insert(0, os.path.abspath( os.path.join(os.path.dirname(__file__), '../src/') ))
+
+from clinical_proxy_tables import generate_proxy_table, cli
+from common.sparksession import SparkConfig
+
+source_file = 'tests/testdata/data/clinical/test_clinical_patients.tsv'
+source_file_err = 'tests/testdata/data/clinical/test_clinical_patients_error.tsv'
+destination_dir = 'tests/testdata/data/clinical/patients'
+
+
+@pytest.fixture(autouse=True)
+def spark():
+    print('------setup------')
+    spark = SparkConfig().spark_session('test-clinical-proxy-preprocessing', 'local[2]')
+    yield spark
+
+    print('------teardown------')
+    clinical_proxy_table = os.path.join(destination_dir)
+    if os.path.exists(clinical_proxy_table):
+        shutil.rmtree(clinical_proxy_table)
+
+
+def test_generate_proxy_table(spark):
+    generate_proxy_table(source_file, destination_dir, spark)
+
+    df = spark.read.format('delta').load(destination_dir)
+    assert df.count() == 3
+
+
+def test_generate_proxy_table_error(spark):
+    with pytest.raises(Exception, match=r'Make sure input file is a valid tab delimited csv file'):
+        generate_proxy_table(source_file_err, destination_dir, spark)
+
+
+def test_cli(spark):
+    runner = CliRunner()
+    result = runner.invoke(cli, ['-m', 'local[2]', '-s', source_file, '-d', destination_dir])
+    assert result.exit_code == 0
+
+    df = spark.read.format('delta').load(destination_dir)
+    assert df.count() == 3
+
