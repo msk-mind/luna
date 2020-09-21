f4b30b7761e246ce1ea84a4233e45520ad47dadf
Aukerman <ataukerman@gmail.com>
2020-09-21 10:53:16 -0400

    Using where clause now, more cleanup and streamlining

--- data_processing/process_scan_job.py
+++ data_processing/process_scan_job.py
@@ -13,228 +13,161 @@ Parameters:
 """
 import sys, os, subprocess, argparse
 
-import pydoop
-import pydoop.hdfs as hdfs
+import click
 import socket
 
 from data_processing.common.Neo4jConnection import Neo4jConnection
+from data_processing.common.sparksession import SparkConfig
 from data_processing.common.custom_logger import init_logger
+from pyspark import SQLContext, SparkContext
+from pyspark.sql import functions as F
+from pyspark.sql.types import StringType,StructType,StructField
 
 logger = init_logger()
+logger.info("Starting process_scan_job.py")
 
 # pydoop.hdfs.cp("file:///Users/aukermaa/DB/test.txt", "/Users/aukermaa/")
 
 @click.command()
 # TODO: query?
-@click.option('-q', '--query', default = None, help = "where clause of SQL query to filter feature table, 'WHERE' does not need to be included, but make sure to wrap with quotes to be interpretted correctly")
+@click.option('-q', '--query', default = None, help = "where clause of CYPHER query to filter ID table, 'WHERE' does not need to be included, wrap with double quotes")
 @click.option('-b', '--base_directory', type=click.Path(exists=True), default="/gpfs/mskmindhdp_emc/", help="location to find scan/annotation tables and to create feature table")
 @click.option('-s', '--spark_master_uri', help='spark master uri e.g. spark://master-ip:7077 or local[*]', required=True)
-@click.option('-g', '--graph_uri', help='spark master uri e.g. bold://host:7883', required=True)
+@click.option('-g', '--graph_uri', help='spark master uri e.g. bolt://localhost:7883', required=True)
+@click.option('-d', '--hdfs_uri', help='hdfs URI uri e.g. hdfs://localhost:8020', required=True)
 @click.option('-c', '--custom_preprocessing_script', default = None, help="Path to python file to execute in the working directory")
 @click.option('-t', '--tag', default = 'default', help="Provencence tag")
-def cli(spark_master_uri, base_directory, target_spacing, query, feature_table_output_name, custom_preprocessing_script):
+def cli(spark_master_uri, base_directory, query, hdfs_uri, graph_uri, custom_preprocessing_script, tag):
     """
     This module ....
 
     Example: python3 ...    """
     # Setup Spark context
+    print (query)
     import time
     start_time = time.time()
     spark = SparkConfig().spark_session("dicom-to-scan", spark_master_uri)
-    generate_feature_table(base_directory, target_spacing, spark, query, feature_table_output_name, custom_preprocessing_script)
-    print("--- Finished in %s seconds ---" % (time.time() - start_time))
+    generate_scan_table(base_directory, spark, query, graph_uri, hdfs_uri, custom_preprocessing_script, tag)
+    logger.info("--- Finished in %s seconds ---" % (time.time() - start_time))
 
 
+def generate_scan_table(base_directory, spark, query, graph_uri, hdfs_uri, custom_preprocessing_script, tag):
+    os.environ["PYSPARK_PYTHON"]="/usr/local/bin/python3"
+    os.environ["PYSPARK_DRIVER_PYTHON"]="/usr/local/bin/python3"
+
+    sc = spark
+    sqlc =  SQLContext(spark)
+
+    logger.info ("-------------------------------------- SETUP COMPLETE -------------------------------------------")
+
+    # We identify scan concepts with the series instance uid
+    concept_id_TYPE = "SeriesInstanceUID"
+    SPARK_SCRATCH_SPACE = "/Users/aukermaa/DB/working/"
+
+    # Open a connection to the ID graph database
+    logger.info (f'''Conncting to uri={graph_uri}, user="neo4j", pwd="password" ''')
+    conn = Neo4jConnection(uri=graph_uri, user="neo4j", pwd="password")
+
+    # Begin query/commute process
+    # logger.info (f" >>> Looking for ID {query_id} of type [{query_type}]")
+    df_driver_ids = conn.commute_source_id_to_spark_query(spark, sqlc, WHERE_CLAUSE=query, SINK_TYPE=concept_id_TYPE)
+
+    logger.info (" >>> Graph Query Complete:")
+    df_driver_ids.show()
+
+    hdfs_db_root = os.environ["MIND_ROOT_DIR"]
+    gpfs_host = 'localhost'
+    gpfs_mount = '/'
+    # Reading dicom and opdata
+    df_dcmdata = sqlc.read.format("delta").load( hdfs_uri + os.path.join(hdfs_db_root, "radiology/tables/radiology.dcm"))
+    df_optdata = sqlc.read.format("delta").load( hdfs_uri + os.path.join(hdfs_db_root, "radiology/tables/radiology.dcm_op"))
+    logger.info (" >>> Loaded dicom DB")
+
+    def python_def_generate_mhd(concept_id, input_paths, filenames):
+            '''
+            Accepts and input of paths, filenames, moves to a temporary working directory, and generates a volumetric MHD filename
+            Args:
+                    input_paths: list of input paths on HDFS Cluster
+                    filenames: list of dcm filenames
+            Returns:
+                    scan_record_uuid - UUID of the request process
+            Notes:
+                    Zero error checking, incomplete
+            '''
+
+            import glob
+            import shutil
+            import os
+            import uuid
+            import subprocess
+            import sys
+            from paramiko import SSHClient
+            from scp import SCPClient
+
+            sys.path.insert(0, '/usr/local/Cellar/hadoop/3.2.1_1/bin/')
+
+            scan_record_uuid  = "SCAN-" + str(uuid.uuid4())
+
+            # Initialize a working directory
+            WORK_DIR   = os.path.join(SPARK_SCRATCH_SPACE, scan_record_uuid)
+            OUTPUT_DIR = os.path.join(WORK_DIR, 'outputs')
+            INPUTS_DIR = os.path.join(WORK_DIR, 'inputs')
+            os.makedirs(WORK_DIR)
+            os.makedirs(OUTPUT_DIR)
+            os.makedirs(INPUTS_DIR)
 
 
-parser = argparse.ArgumentParser(description='Submission script to run dicom-to-scan concept transformations')
-parser.add_argument('--id', dest='id', type=str, help='Specify query ID (ex. DMP-0001)')
-parser.add_argument('--type', dest='type', type=str, help='Specify query ID type (ex. dmp_patient_id')
-parser.add_argument('--hdfs', dest='hdfs', type=str, help='HDFS host')
-parser.add_argument('--db', dest='db', type=str, help='DB root directory (ex. /DB/')
-parser.add_argument('--gpfs', dest='gpfs', type=str, help='GPFS host')
-parser.add_argument('--mount', dest='mount', type=str, help='GPFS mount')
-parser.add_argument('--tag', dest='tag', type=str, help='Provencence Tag')
-args = parser.parse_args()
+            # # Data pull request into temporary working directory
+            # # TODO:  Will actally be much more streamlined as binary data can be read-out from proxy table
+            pull_req_dcm = [ os.path.join(path,file) for path, file in zip(input_paths, filenames)]
 
-print (os.getlogin())
+            #for dcm in pull_req_dcm: shutil.copy(dcm, WORK_DIR)
+            ssh = SSHClient()
+            ssh.load_system_host_keys()
+            ssh.connect(gpfs_host)
+            with SCPClient(ssh.get_transport()) as scp:
+                for dcm in pull_req_dcm:
+                    scp.get(gpfs_mount + dcm, INPUTS_DIR)
+                # Awful and slow bc it spins up a new jvm 100 times per scan being processed
+                # subprocess.call(['/bin/sh', '/usr/local/bin/hadoop', 'fs', '-get', hdfs_uri + dcm, WORK_DIR])
+                # pydoop.hdfs.cp( os.path.join(hdfs_uri, dcm), WORK_DIR )
 
-query_type = args.type
-query_id   = args.id
-custom_preprocessing_script   = args.py
-spark_master_uri   = args.spark
-hdfs_host  = args.hdfs
-hdfs_db_root = args.db
-graph_host      = args.graph
-gpfs_mount = args.mount
-gpfs_host = args.gpfs
-tag_user  = args.tag
+            # Execute some modularized python script
+            # Expects intputs at WORK_DIR, puts outputs into WORK_DIR/outputs
+            proc = subprocess.Popen(["/usr/local/bin/python3", custom_preprocessing_script, WORK_DIR], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = proc.communicate()
 
-# subprocess to find the java , scala and python version
-cmd1 = "java -version"
-cmd2 = "scala -version"
-cmd3 = "python3 --version"
+            # Send write message to scan io server
+            # Message format is 5 arguements [command], [working directory path], [concept ID to attach], [record ID to ingest], [tag]
+            message = ','.join(["WRITE", OUTPUT_DIR, concept_id, scan_record_uuid, tag])
+            client_socket = socket.socket()  # instantiate
+            client_socket.setblocking(1)
+            client_socket.connect(("localhost", 5090))  # connect to the server
+            client_socket.send(message.encode())  # send message
+            client_socket.close()  # close the connection
 
-arr = [cmd1, cmd2, cmd3]
+            # Were all done here, the write service takes care of the rest!!!
+            # Returning record ID
+            return scan_record_uuid
 
-for cmd in arr:
-    print (cmd)
-    subprocess.run (cmd.split(" "))
+    # Make our UDF
+    udf_generate_mhd = F.udf(python_def_generate_mhd, StringType())
 
-print("JAVA_HOME=", os.getenv("JAVA_HOME"))
-print("HADOOP_HOME=", os.getenv("HADOOP_HOME"))
-print("SPARK_HOME=", os.getenv("SPARK_HOME"))
-print("HOME=",      os.getenv("HOME"))
+    # Get ready to run UDF jobs
+    df_get = df_dcmdata.join(df_optdata , ["dicom_record_uuid"]).join(df_driver_ids, [concept_id_TYPE]) \
+        .select(concept_id_TYPE,"absolute_hdfs_path","filename") \
+        .groupBy(concept_id_TYPE) \
+        .agg(F.sort_array( F.collect_list("absolute_hdfs_path")).alias("absolute_hdfs_paths"), \
+             F.sort_array( F.collect_list("filename")).alias("filenames") )
 
-# Setup spark context and processing
-os.environ["PYSPARK_PYTHON"]="/usr/local/bin/python3"
-os.environ["PYSPARK_DRIVER_PYTHON"]="/usr/local/bin/python3"
+    logger.info (" >>> Job queue:")
+    df_get.show()
 
-from pyspark.sql import SparkSession
-from pyspark import SparkConf
-from pyspark import SparkContext
-from pyspark import SQLContext
-from pyspark.sql import functions as F
-from pyspark.sql.types import StringType,StructType,StructField
-import pyarrow as pa
-conf = SparkConf()\
-    .setAppName("generate_ct") \
-    .setMaster(spark_master_uri) \
-    .set("spark.jars.packages", "io.delta:delta-core_2.12:0.7.0") \
-    .set("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-    .set("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-    .set("spark.driver.bindAddress", "0.0.0.0") \
-    .set("spark.driver.memory", "4g") \
-    .set("spark.cores.max", "8") \
-    .set("spark.network.timeout", "600s")
-
-sc = SparkContext(conf=conf)
-sqlc =  SQLContext(sc)
-print ("SPARK VERSION ", sc.version)
-
-print ("-------------------------------------- SETUP COMPLETE -------------------------------------------")
-
-# We identify scan concepts with the series instance uid
-concept_id_TYPE = "SeriesInstanceUID"
-SPARK_SCRATCH_SPACE = "/Users/aukermaa/DB/working/"
-
-# Open a connection to the ID graph database
-print (f'''Conncting to uri={graph_host}, user="neo4j", pwd="password" ''')
-conn = Neo4jConnection(uri=graph_host, user="neo4j", pwd="password")
-
-# Begin query/commute process
-print (f" >>> Looking for ID {query_id} of type [{query_type}]")
-if query_type == "cohort_id":
-    # This is a cohort ID, so only transverse cohort link once
-    df_driver_ids = conn.commute_cohort_id_to_spark(sc, sqlc, query_type, concept_id_TYPE, query_id )
-elif 'record' in query_type:
-    df_driver_ids = conn.commute_record_id_to_spark(sc, sqlc, query_type, concept_id_TYPE)
-elif query_type == concept_id_TYPE:
-    # The ID given is equal to the concept ID, so no query needed
-    if query_id == "all":
-        df_driver_ids = conn.commute_all_sink_id(sc, sqlc, concept_id_TYPE)
-    else:
-        df_driver_ids = conn.commute_sink_id_to_spark(sc, sqlc, concept_id_TYPE, query_id)
-
-else:
-    # COmmute id
-    df_driver_ids = conn.commute_source_id_to_spark(sc, sqlc, query_type, concept_id_TYPE, query_id)
-
-print (" >>> Graph Query Complete:")
-df_driver_ids.show()
-
-# Reading dicom and opdata
-df_dcmdata = sqlc.read.format("delta").load( hdfs_host + os.path.join(hdfs_db_root, "radiology/tables/radiology.dcm"))
-df_optdata = sqlc.read.format("delta").load( hdfs_host + os.path.join(hdfs_db_root, "radiology/tables/radiology.dcm_op"))
-print (" >>> Loaded dicom DB")
-df_dcmdata.printSchema()
-df_optdata.printSchema()
-
-def python_def_generate_mhd(concept_id, input_paths, filenames):
-        '''
-        Accepts and input of paths, filenames, moves to a temporary working directory, and generates a volumetric MHD filename
-        Args:
-                input_paths: list of input paths on HDFS Cluster
-                filenames: list of dcm filenames
-        Returns:
-                scan_record_uuid - UUID of the request process
-        Notes:
-                Zero error checking, incomplete
-        '''
-
-        import glob
-        import shutil
-        import os
-        import uuid
-        import subprocess
-        import sys
-        from paramiko import SSHClient
-        from scp import SCPClient
-
-        sys.path.insert(0, '/usr/local/Cellar/hadoop/3.2.1_1/bin/')
-
-        scan_record_uuid  = "SCAN-" + str(uuid.uuid4())
-
-        # Initialize a working directory
-        WORK_DIR   = os.path.join(SPARK_SCRATCH_SPACE, scan_record_uuid)
-        OUTPUT_DIR = os.path.join(WORK_DIR, 'outputs')
-        INPUTS_DIR = os.path.join(WORK_DIR, 'inputs')
-        os.makedirs(WORK_DIR)
-        os.makedirs(OUTPUT_DIR)
-        os.makedirs(INPUTS_DIR)
-
-
-        # # Data pull request into temporary working directory
-        # # TODO:  Will actally be much more streamlined as binary data can be read-out from proxy table
-        pull_req_dcm = [ os.path.join(path,file) for path, file in zip(input_paths, filenames)]
-
-        #for dcm in pull_req_dcm: shutil.copy(dcm, WORK_DIR)
-        ssh = SSHClient()
-        ssh.load_system_host_keys()
-        ssh.connect(gpfs_host)
-        with SCPClient(ssh.get_transport()) as scp:
-            for dcm in pull_req_dcm:
-                scp.get(gpfs_mount + dcm, INPUTS_DIR)
-            # Awful and slow bc it spins up a new jvm 100 times per scan being processed
-            # subprocess.call(['/bin/sh', '/usr/local/bin/hadoop', 'fs', '-get', hdfs_host + dcm, WORK_DIR])
-            # pydoop.hdfs.cp( os.path.join(hdfs_host, dcm), WORK_DIR )
-
-        # Execute some modularized python script
-        # Expects intputs at WORK_DIR, puts outputs into WORK_DIR/outputs
-        proc = subprocess.Popen(["/usr/local/bin/python3", custom_preprocessing_script, WORK_DIR], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = proc.communicate()
-
-        # Send write message to scan io server
-        # Message format is 5 arguements [command], [working directory path], [concept ID to attach], [record ID to ingest], [tag]
-        message = ','.join(["WRITE", OUTPUT_DIR, concept_id, scan_record_uuid, tag_user])
-        client_socket = socket.socket()  # instantiate
-        client_socket.setblocking(1)
-        client_socket.connect(("localhost", 5090))  # connect to the server
-        client_socket.send(message.encode())  # send message
-        client_socket.close()  # close the connection
-
-        # Were all done here, the write service takes care of the rest!!!
-        # Returning record ID
-        return scan_record_uuid
-
-# Make our UDF
-udf_generate_mhd = F.udf(python_def_generate_mhd, StringType())
-
-# Get ready to run UDF jobs
-df_get = df_dcmdata.join(df_optdata , ["dicom_record_uuid"]).join(df_driver_ids, [concept_id_TYPE]) \
-    .select(concept_id_TYPE,"absolute_hdfs_path","filename") \
-    .groupBy(concept_id_TYPE) \
-    .agg(F.sort_array( F.collect_list("absolute_hdfs_path")).alias("absolute_hdfs_paths"), \
-         F.sort_array( F.collect_list("filename")).alias("filenames") )
-
-print (" >>> Job queue: ", df_get.count())
-df_get.show()
-
-# Run jobs
-print (" >>> Calling jobs on selected patient:")
-df_ct = df_get.withColumn('payload', udf_generate_mhd(concept_id_TYPE, 'absolute_hdfs_paths', 'filenames'))
-print (" >>> Jobs done: ", df_ct.count())
-df_ct.show()
+    # Run jobs
+    logger.info (" >>> Calling jobs on selected patient:")
+    df_ct = df_get.withColumn('payload', udf_generate_mhd(concept_id_TYPE, 'absolute_hdfs_paths', 'filenames'))
+    logger.info (" >>> Jobs done")
+    df_ct.show()
 
 
 if __name__ == "__main__":
