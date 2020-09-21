485a7845cc213d119a3f478294378c4368ef9567
Aukerman <ataukerman@gmail.com>
2020-09-21 13:02:18 -0400

    Final cleanup

--- data_processing/process_scan_job.py
+++ data_processing/process_scan_job.py
@@ -47,7 +47,6 @@ logger.info("Starting process_scan_job.py")
 # pydoop.hdfs.cp("file:///Users/aukermaa/DB/test.txt", "/Users/aukermaa/")
 
 @click.command()
-# TODO: query?
 @click.option('-q', '--query', default = None, help = "where clause of CYPHER query to filter ID table, 'WHERE' does not need to be included, wrap with double quotes")
 @click.option('-b', '--base_directory', type=click.Path(exists=True), default="/gpfs/mskmindhdp_emc/", help="location to find scan/annotation tables and to create feature table")
 @click.option('-s', '--spark_master_uri', help='spark master uri e.g. spark://master-ip:7077 or local[*]', required=True)
@@ -74,29 +73,23 @@ def generate_scan_table(base_directory, spark, query, graph_uri, hdfs_uri, custo
     os.environ["PYSPARK_DRIVER_PYTHON"]="/usr/local/bin/python3"
     hdfs_db_root    = os.environ["MIND_ROOT_DIR"]
     spark_workspace = os.environ["MIND_WORK_DIR"]
-
-    sc = spark
-    sqlc =  SQLContext(spark)
-
-    logger.info ("-------------------------------------- SETUP COMPLETE -------------------------------------------")
-
-    # We identify scan concepts with the series instance uid
     concept_id_TYPE = "SeriesInstanceUID"
+    gpfs_host = 'localhost'
+    gpfs_mount = '/'
 
     # Open a connection to the ID graph database
     logger.info (f'''Conncting to uri={graph_uri}, user="neo4j", pwd="password" ''')
     conn = Neo4jConnection(uri=graph_uri, user="neo4j", pwd="password")
 
-    # Begin query/commute process
-    # logger.info (f" >>> Looking for ID {query_id} of type [{query_type}]")
-    df_driver_ids = conn.commute_source_id_to_spark_query(spark, sqlc, WHERE_CLAUSE=query, SINK_TYPE=concept_id_TYPE)
+    sqlc =  SQLContext(spark)
 
+    logger.info ("-------------------------------------- SETUP COMPLETE -------------------------------------------")
+
+    # Begin query/commute process
+    df_driver_ids = conn.commute_source_id_to_spark_query(spark, sqlc, WHERE_CLAUSE=query, SINK_TYPE=concept_id_TYPE)
     logger.info (" >>> Graph Query Complete:")
     df_driver_ids.show()
 
-
-    gpfs_host = 'localhost'
-    gpfs_mount = '/'
     # Reading dicom and opdata
     df_dcmdata = sqlc.read.format("delta").load( hdfs_uri + os.path.join(hdfs_db_root, "radiology/tables/radiology.dcm"))
     df_optdata = sqlc.read.format("delta").load( hdfs_uri + os.path.join(hdfs_db_root, "radiology/tables/radiology.dcm_op"))
@@ -164,18 +157,15 @@ def generate_scan_table(base_directory, spark, query, graph_uri, hdfs_uri, custo
     udf_generate_mhd = F.udf(python_def_generate_mhd, StringType())
 
     # Get ready to run UDF jobs
-    df_get = df_dcmdata.join(df_optdata , ["dicom_record_uuid"]).join(df_driver_ids, [concept_id_TYPE]) \
+    df_queue= df_dcmdata.join(df_optdata , ["dicom_record_uuid"]).join(df_driver_ids, [concept_id_TYPE]) \
         .select(concept_id_TYPE,"absolute_hdfs_path","filename") \
         .groupBy(concept_id_TYPE) \
         .agg(F.sort_array( F.collect_list("absolute_hdfs_path")).alias("absolute_hdfs_paths"), \
              F.sort_array( F.collect_list("filename")).alias("filenames") )
 
-    logger.info (" >>> Job queue:")
-    df_get.show()
-
     # Run jobs
     logger.info (" >>> Calling jobs on selected patient:")
-    df_ct = df_get.withColumn('payload', udf_generate_mhd(concept_id_TYPE, 'absolute_hdfs_paths', 'filenames'))
+    df_ct = df_queue.withColumn('payload', udf_generate_mhd(concept_id_TYPE, 'absolute_hdfs_paths', 'filenames'))
     logger.info (" >>> Jobs done")
     df_ct.show()
 
