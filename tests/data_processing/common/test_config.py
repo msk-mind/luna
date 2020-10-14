e68db4dbca5cb05ffdffadd54bf1c70f410382e8
arfath pasha <arfathpasha@gmail.com>
2020-10-14 12:10:42 -0400

    add singleton test

--- tests/data_processing/common/test_config.py
+++ tests/data_processing/common/test_config.py
@@ -5,21 +5,32 @@ Created on October 17, 2019
 
 @author: pashaa@mskcc.org
 '''
+from data_processing.common.config import Config
 
-
-def test_get_value():
-    from data_processing.common.config import Config
-
+def test_singleton():
     c1 = Config('tests/data_processing/common/test_config.yaml')
     c2 = Config('tests/data_processing/common/test_config.yaml')
-    c3 = Config('tests/data_processing/common/test_config.yaml')
 
-    print(str(c1) + ' ' + str(c1.get_value('$.spark_application_config[:1]["spark.executor.cores"]')))
-    print(str(c2) + ' ' + str(c1.get_value('$.spark_application_config[:1]["spark.executor.cores"]')))
-    print(str(c3) + ' ' + str(c1.get_value('$.spark_application_config[:1]["doesnt_exist"]')))
+    assert c1 == c2  # instance is reused for the same config.yaml
 
     assert c1.get_value('$.spark_application_config[:1]["spark.executor.cores"]') == 111
     assert c2.get_value('$.spark_application_config[:1]["spark.executor.cores"]') == 111
+
+    c3 = Config('tests/data_processing/common/another_test_config.yaml')
+    c4 = Config('tests/data_processing/common/another_test_config.yaml')
+
+    assert c3 == c4 # but new instance is created for a new config yaml
+    assert c2 != c3
+
+    assert c3.get_value('$.spark_application_config[:1]["spark.executor.cores"]') == 222
+    assert c4.get_value('$.spark_application_config[:1]["spark.executor.cores"]') == 222
+
+
+def test_get_value():
+    c1 = Config('tests/data_processing/common/test_config.yaml')
+    c3 = Config('tests/data_processing/common/test_config.yaml')
+
+    assert c1.get_value('$.spark_application_config[:1]["spark.executor.cores"]') == 111
     assert c3.get_value('$.spark_application_config[:1]["doesnt.exist"]') == None
 
 
