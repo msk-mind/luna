1a4a9a57ea66a70a04a278f3498531089c7210dd
arfath pasha <arfathpasha@gmail.com>
2020-11-04 00:03:38 -0500

    polish transfer files

--- data_processing/radiology/proxy_table/transfer_files.sh
+++ data_processing/radiology/proxy_table/transfer_files.sh
@@ -1,18 +1,22 @@
-LOG_FILE=rsync.log
+########################### setup ###########################
+
+LOG_FILE=transfer_files.log
 echo ">>>> writing data transfer logs to $LOG_FILE..."
 echo "Running data_processing/radiology/proxy_table/transfer_files.sh with $1" >> $LOG_FILE;
 exit_code=0
 
-# TODO: verify data ingestion template
+# TODO: validate data ingestion template
 
 # set num_procs equal to magnitude of bandwidth.
-# i.e. num_proc = bwlimit with last character stripped out
-bw=$BWLIMIT
-num_procs=${bw%?}
+# i.e. num_proc = bwlimit with last character stripped
+bw = $BWLIMIT
+num_procs = ${bw%?}
+
+########################### transfer ###########################
 
 # TODO: looks like the -h option can be removed, it is for help
 # transfer whole files without delta-xfer algorithm from specified source location to destination location.
-# spawn one process per unit of network bandwidth.
+# spawn one process per unit of network bandwidth
 # delete any files in the destination location that are not in the source location
 # output stats at the end
 # output a log file
@@ -22,30 +26,34 @@ time cat $CHUNK_FILE | xargs -I {} -P $num_procs -n 1 \
 rsync -ahW --delete --stats --log-file=$LOG_FILE \
 --exclude '*.'{$EXCLUDES} \
 --bwlimit=$BWLIMIT  \
-$HOST:$SOURCE_PATH/{} $DESTINATION_PATH/
+$HOST:$SOURCE_PATH/{} $DESTINATION_PATH
 
-let exit_code=$?+$exit_code
+let exit_code = $? + $exit_code
 echo "exit code after rsync = $exit_code" >> $LOG_FILE
 
+########################### verify ###########################
+
 # verify and log and log file counts
-file_count=$(find mskmind_XNAT -type  f  -name "*" | wc -l)
+file_count = $(find mskmind_XNAT -type f -name "*" | wc -l)
 [ $FILE_COUNT == $file_count ];
 
-let exit_code=$?+$exit_code
+let exit_code = $? + $exit_code
 echo "exit code after file_count verification = $exit_code" >> $LOG_FILE
 
 # verify and log transfer data size (bytes)
-data_size=$(find $DESTINATION_PATH -type f -name "*" | xargs du -ac)
+data_size = $(find $DESTINATION_PATH -type f -name "*" | xargs du -ac)
 [ $FILE_COUNT == $file_count ];
 
-let exit_code=$?+$exit_code
+let exit_code = $?+$exit_code
 echo "exit code after data_size verification = $exit_code" >> $LOG_FILE
 
 # write manifest file
 cp $1 $DESTINATION_PATH/manifest.yaml
 
-let exit_code=$?+$exit_code
+let exit_code = $? + $exit_code
 echo "exit code after write manifest file = $exit_code" >> $LOG_FILE
+
+########################### teardown ###########################
 
 # the end
 if exit_code
