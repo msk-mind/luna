adff2d64200f563f1a9ef351502dfc67ea122d45
arfath pasha <arfathpasha@gmail.com>
2020-11-05 01:30:43 -0500

    add unit tests and fix script

--- data_processing/radiology/proxy_table/transfer_files.sh
+++ data_processing/radiology/proxy_table/transfer_files.sh
@@ -1,41 +1,53 @@
-########################### setup ###########################
-
+########################### setup pre-conditions ###########################
+set -x
 LOG_FILE=transfer_files.log
-echo ">>>> writing data transfer logs to $LOG_FILE..."
-echo "Running data_processing/radiology/proxy_table/transfer_files.sh with $1" >> $LOG_FILE;
 exit_code=0
 
-# validate env vars
-[ "${BWLIMIT}" ]
-let exit_code = $? + $exit_code
-[ "${CHUNK_FILE}" ]
-let exit_code = $? + $exit_code
-[ "${EXCLUDES}" ]
-let exit_code = $? + $exit_code
-[ "${HOST}" ]
-let exit_code = $? + $exit_code
-[ "${SOURCE_PATH}" ]
-let exit_code = $? + $exit_code
-[ "${DESTINATION_PATH}" ]
-let exit_code = $? + $exit_code
-[ "${FILE_COUNT}" ]
-let exit_code = $? + $exit_code
-[ "${DATA_SIZE}" ]
-let exit_code = $? + $exit_code
+echo ">>>> writing data transfer logs to $LOG_FILE..."
+echo "Running data_processing/radiology/proxy_table/transfer_files.sh with - " >> $LOG_FILE;
+echo "BWLIMIT = $BWLIMIT" >> $LOG_FILE;
+echo "CHUNK_FILE = $CHUNK_FILE" >> $LOG_FILE;
+echo "EXCLUDES = $EXCLUDES" >> $LOG_FILE;
+echo "HOST = $HOST" >> $LOG_FILE;
+echo "SOURCE_PATH = $SOURCE_PATH" >> $LOG_FILE;
+echo "DESTINATION_PATH = $DESTINATION_PATH" >> $LOG_FILE;
+echo "FILE_COUNT = $FILE_COUNT" >> $LOG_FILE;
+echo "DATA_SIZE = $DATA_SIZE" >> $LOG_FILE;
 
-if exit_code
+# todo: make keys in ingestion template all caps
+
+# validate env vars - check if they exist
+[ "${BWLIMIT}" ]
+let exit_code=$?+$exit_code
+[ "${CHUNK_FILE}" ]
+let exit_code=$?+$exit_code
+[ "${HOST}" ]
+let exit_code=$?+$exit_code
+[ "${SOURCE_PATH}" ]
+let exit_code=$?+$exit_code
+[ "${DESTINATION_PATH}" ]
+let exit_code=$?+$exit_code
+[ "${FILE_COUNT}" ]
+let exit_code=$?+$exit_code
+[ "${DATA_SIZE}" ]
+let exit_code=$?+$exit_code
+
+if [ $exit_code -eq 0 ]
 then
-        { echo "validated environment variables" }
+  { echo "\nvalidated environment variables..." >> $LOG_FILE; }
 else
-        { echo "file transfer failed. missing required environment variables." ; exit 1 ; }
+  { echo "\nERROR: file transfer failed. missing required environment variables!" >> $LOG_FILE; exit 1 ; }
 fi
+
+# create destination dir if it does not exist
+mkdir -p $DESTINATION_PATH
 
 # set num_procs equal to magnitude of bandwidth.
 # i.e. num_proc = bwlimit with last character stripped
-bw = $BWLIMIT
-num_procs = ${bw%?}
+bw=$BWLIMIT
+num_procs=${bw%?}
 
-########################### transfer ###########################
+########################### transfer files ###########################
 
 # TODO: looks like the -h option can be removed, it is for help
 # transfer whole files without delta-xfer algorithm from specified source location to destination location.
@@ -51,37 +63,35 @@ rsync -ahW --delete --stats --log-file=$LOG_FILE \
 --bwlimit=$BWLIMIT  \
 $HOST:$SOURCE_PATH/{} $DESTINATION_PATH
 
-let exit_code = $? + $exit_code
+let exit_code=$?+$exit_code
 echo "exit code after rsync = $exit_code" >> $LOG_FILE
 
-########################### verify ###########################
+########################### verify post-conditions ###########################
 
 # verify and log and log file counts
-file_count = $(find mskmind_XNAT -type f -name "*" | wc -l)
-[ $FILE_COUNT == $file_count ];
+file_count=$(find $DESTINATION_PATH -type f -name "*" | wc -l)
+[ $FILE_COUNT -eq $file_count ];
 
-let exit_code = $? + $exit_code
+let exit_code=$?+$exit_code
 echo "exit code after file_count verification = $exit_code" >> $LOG_FILE
 
 # verify and log transfer data size (bytes)
-data_size = $(find $DESTINATION_PATH -type f -name "*" | xargs du -ac)
-[ $DATA_SIZE == $data_size ];
-
-let exit_code = $?+$exit_code
-echo "exit code after data_size verification = $exit_code" >> $LOG_FILE
-
-# write manifest file
-cp $1 $DESTINATION_PATH/manifest.yaml
+data_size=$(find $DESTINATION_PATH -type d | xargs du -s | cut -f1)
+[ $DATA_SIZE -eq $data_size ];
 
 let exit_code = $? + $exit_code
-echo "exit code after write manifest file = $exit_code" >> $LOG_FILE
+echo "exit code after data_size verification = $exit_code" >> $LOG_FILE
 
 ########################### teardown ###########################
 
 # the end
-if exit_code
+echo "\n" >> $LOG_FILE
+echo "=============================================================" >> $LOG_FILE
+echo "\n" >> $LOG_FILE
+
+if [ $exit_code -eq 0 ]
 then
         { echo "file transfer completed successfully!" ; exit 0 ; }
 else
-        { echo "file transfer failed. See $LOG_FILE" ; exit 1 ; }
+        { echo "file transfer failed! See $LOG_FILE" ; exit 1 ; }
 fi
---
+++ tests/data_processing/radiology/proxy_table/test_transfer_files.py
@@ -0,0 +1,73 @@
+'''
+Created on November 04, 2020
+
+@author: pashaa@mskcc.org
+'''
+import os
+import shutil
+import subprocess
+
+import pytest
+
+'''
+NOTE: These unit tests have been deliberately commented out.
+ 
+To execute these unit tests, follow these steps to allow localhost rsync and then uncomment tests to run them
+ 
+1. Copy your public key (typically at ~/.ssh/is_rsa.pub) to your authorized_keys file (~/.ssh/authorized_keys)
+
+2. If your private key has a passphrase associated with it, override it using the ssh-agent, but first check if the  agent if running on your machine  with
+
+$ eval `ssh-agent`
+
+If it is running, then add the ssh key to the agent with ssh-add and enter your passphrase when prompted for it.
+$ ssh-add
+
+Now the terminal will not require a passhprase for any subsequent executions
+
+3. If the ssh-agent is not running, then start the ssh-agent with the command below
+
+$ ssh-agent bash
+'''
+
+
+
+@pytest.fixture(autouse=True)
+def env():
+    print('------setup------')
+    os.environ['BWLIMIT'] = '1G'
+    os.environ['CHUNK_FILE'] = 'tests/data_processing/radiology/proxy_table/test_data/chunk_file.txt'
+    os.environ['HOST'] = '127.0.0.1'
+    os.environ['SOURCE_PATH'] = os.getcwd()+\
+                                '/tests/data_processing/radiology/proxy_table/test_data/source'
+    os.environ['DESTINATION_PATH'] = os.getcwd()+\
+                                     '/tests/data_processing/radiology/proxy_table/test_data/destination'
+
+
+    yield env
+
+    print('------teardown------')
+    shutil.rmtree(os.environ.get('DESTINATION_PATH'))
+
+
+'''
+def test_transfer_files(env):
+    os.environ['EXCLUDES'] = ''
+    os.environ['FILE_COUNT'] = '4'
+    os.environ['DATA_SIZE'] = '32'
+
+    transfer_cmd = ["time", "./data_processing/radiology/proxy_table/transfer_files.sh"]
+
+    try:
+        exit_code = subprocess.call(transfer_cmd)
+    except Exception as err:
+        pytest.fail(msg="Error Transfering files with rsync" + str(err))
+
+
+    assert(exit_code == 0)
+    assert os.path.exists(os.getenv('DESTINATION_PATH'))
+    assert os.path.exists(os.getenv('DESTINATION_PATH')+'/test1.dcm')
+    assert os.path.exists(os.getenv('DESTINATION_PATH')+'/test1.mha')
+    assert os.path.exists(os.getenv('DESTINATION_PATH')+'/test1.mhd')
+    assert os.path.exists(os.getenv('DESTINATION_PATH')+'/test1.raw')
+'''
