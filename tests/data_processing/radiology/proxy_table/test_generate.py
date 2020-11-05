586fe435c11359e8524a46fb25c607e5617cc53e
doori <doori.rose@gmail.com>
2020-11-05 17:06:36 -0500

    unittest for radiology proxy generate

---
+++ tests/data_processing/radiology/proxy_table/test_generate.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+import subprocess
+
+import pytest
+from click.testing import CliRunner
+from data_processing.radiology.proxy_table.generate import *
+from data_processing.common.sparksession import SparkConfig
+
+
+# TABLE_PATH in tests/data_processing/data_ingestion_template_valid.yaml
+dicom_table_path = "tests/data_processing/radiology/proxy_table/OV_16-158_CT_20201028/table/"
+test_ingestion_template = "tests/data_processing/radiology/proxy_table/OV_16-158_CT_20201028/data_ingestion_template_valid.yaml"
+
+@pytest.fixture(autouse=True)
+def spark():
+    print('------setup------')
+    spark = SparkConfig().spark_session('tests/data_processing/common/test_config.yaml',
+                                        'test-radiology-proxy')
+    yield spark
+
+    print('------teardown------')
+    if os.path.exists(dicom_table_path):
+        shutil.rmtree(dicom_table_path)
+    os.remove(test_ingestion_template)
+
+
+def test_cli(spark):
+
+    runner = CliRunner()
+    result = runner.invoke(cli, 
+        ['-t', 'tests/data_processing/data_ingestion_template_valid.yaml', 
+        '-f', 'tests/data_processing/common/test_config.yaml', 
+        '-s', 'true'])
+
+    print(result.exc_info)
+    assert result.exit_code == 0
+
+    df = spark.read.format("delta").load(dicom_table_path + "dicom_header")
+    assert df.count() == 1
+    assert "dicom_record_uuid" in df.columns
+
+
