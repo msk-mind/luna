a5b84edbffaa94ab0561169dfe7d279d2b33caf7
Aukerman <aukermaa@pllimsksparky1.mskcc.org>
2020-11-12 17:27:59 -0500

    Graph basically works

--- data_processing_app.py
+++ data_processing_app.py
@@ -8,6 +8,7 @@ from data_processing.common.sparksession import SparkConfig
 from data_processing.common.Neo4jConnection import Neo4jConnection
 
 app = Flask(__name__)
+logger = init_logger("flask-mind-server.log")
 
 @app.route('/')
 def index():
@@ -37,11 +38,12 @@ def graph():
     prop_string = ','.join(['''{0}: "{1}"'''.format(prop, data[prop]) for prop in data.keys()])
     conn.query(f'''MERGE (n:dataset{{{prop_string}}})''')
 
-    return f'''MERGE (n:dataset{{{prop_string}}})'''
-
     with CodeTimer(logger, 'setup proxy table'):
         # Reading dicom and opdata
-        df_dcmdata = spark.read.format("delta").load(dicom_header_path)
+        try:
+            df_dcmdata = spark.read.format("delta").load(dicom_header_path)
+        except:
+            return (f"{dicom_header_path} either does not exist or is not a delta table") 
 
         tuple_to_add = df_dcmdata.select("PatientName", "SeriesInstanceUID")\
             .groupBy("PatientName", "SeriesInstanceUID")\
@@ -51,9 +53,9 @@ def graph():
     with CodeTimer(logger, 'syncronize graph'):
 
         for index, row in tuple_to_add.iterrows():
-            query ='''MATCH (das:dataset {{DATASET_NAME: "{0}"}}) MERGE (px:xnat_patient_id {{value: "{1}"}}) MERGE (sc:scan {{SeriesInstanceUID: "{2}"}}) MERGE (px)-[r1:HAS_SCAN]->(sc) MERGE (das)-[r2:HAS_PX]-(px)'''.format(os.environ['DATASET_NAME'], row['PatientName'], row['SeriesInstanceUID'])
+            query ='''MATCH (das:dataset {{DATASET_NAME: "{0}"}}) MERGE (px:xnat_patient_id {{value: "{1}"}}) MERGE (sc:scan {{SeriesInstanceUID: "{2}"}}) MERGE (px)-[r1:HAS_SCAN]->(sc) MERGE (das)-[r2:HAS_PX]-(px)'''.format(data['DATASET_NAME'], row['PatientName'], row['SeriesInstanceUID'])
             logger.info (query)
             conn.query(query)
-
+    return (f"Dataset {data['DATASET_NAME']} added successfully!")
 if __name__ == '__main__':
     app.run(host = os.environ['HOSTNAME'], debug=True)
