abb29603301583417dc38ac73f649c466265750b
Elsherif <elsherie@lm620004.mskcc.root.mskcc.org>
2020-11-17 15:50:32 -0500

    transferFiles microservice

---
+++ data_processing/transferFiles/transferFiles.py
@@ -0,0 +1,82 @@
+#!/gpfs/mskmindhdp_emc/sw/env/bin/python3
+
+"""
+To start a server: ./data_processing_app.py (Recommended on sparky1)
+"""
+
+from flask import Flask, request
+import os
+import sys
+
+sys.path.append(os.path.abspath('../'))
+
+from common.CodeTimer import CodeTimer
+from common.custom_logger import init_logger
+from common.sparksession import SparkConfig
+from common.Neo4jConnection import Neo4jConnection
+import importlib
+
+
+from pyspark.sql.functions import udf, lit
+from pyspark.sql.types import StringType
+
+import pydicom
+import time
+from io import BytesIO
+import os, shutil, sys, importlib
+import json
+import yaml, os
+import subprocess
+from filehash import FileHash
+from distutils.util import strtobool
+
+app = Flask(__name__)
+logger = init_logger("flask-mind-server.log")
+# spark = SparkConfig().spark_session(os.environ['SPARK_CONFIG'], "data_processing.mind.api")
+
+
+@app.route('/')
+def index():
+    # setup env variables
+    return "Hello, MSK!"
+
+
+"""
+Transfer Radiology files
+"""
+
+@app.route('//mind/api/v1/transferFiles', methods=['GET', 'POST'])
+def transferFiles():
+    start_time = time.time()
+    setup_environment_from_yaml(os.environ['PATH_TO_TEMPLATE_FILE'])
+    transfer_cmd = ["time", "../data_processing/radiology/proxy_table/transfer_files.sh"]
+
+    try:
+        exit_code = subprocess.call(transfer_cmd)
+        logger.info("--- Finished transferring files in %s seconds ---" % (time.time() - start_time))
+    except Exception as err:
+        logger.error(("Error Transferring files with rsync" + str(err)))
+        return
+
+
+    if exit_code != 0:
+        logger.error(("Error Transfering files - Non-zero exit code: " + str(exit_code)))
+        return "Radiology files transfer is unsuccessful"
+
+    return "Radiology files transfer is successful"
+
+def setup_environment_from_yaml(template_file):
+    # read template_file yaml and set environmental variables for subprocesses
+    with open(template_file, 'r') as template_file_stream:
+        template_dict = yaml.safe_load(template_file_stream)
+
+    logger.info(template_dict)
+
+    # add all fields from template as env variables
+    for var in template_dict:
+        os.environ[var] = str(template_dict[var]).strip()
+
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ['HOSTNAME'], debug=True)
