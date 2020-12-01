fbb3c15792bbe1e3bf2b8e88cbfda21cb73a169e
doori <doori.rose@gmail.com>
2020-12-01 11:40:34 -0500

    unittests work

--- data_processing/services/graph_service.py
+++ data_processing/services/graph_service.py
@@ -1,39 +1,48 @@
-from data_processing.common import GraphEnum, Neo4jConnection, custom_logger
+import os
+import click
+import pandas as pd
+from pyspark.sql import functions as F
+
+from data_processing.common.GraphEnum import GraphEnum
+from data_processing.common.Neo4jConnection import Neo4jConnection
+from data_processing.common.custom_logger import init_logger
 from data_processing.common.sparksession import SparkConfig
+from data_processing.common.config import ConfigSet
 import data_processing.common.constants as const
 
-# TODO test with test-project.
 @click.command()
 @click.option('-p', '--project', help="project name", required=True)
 @click.option('-t', '--table', help="table name", required=True)
 @click.option('-f', '--config_file', default = 'config.yaml', required=True,
               help="path to config file containing application configuration. See config.yaml.template")
-def update_graph(project_name, table_name, config_file):
+def update_graph(project, table, config_file):
 
 	logger = init_logger('update_graph.log')
 
 	# Set up : Neo4j connection and Spark session
 	logger.info("Setting up graph connection and spark session")
-	GRAPH_URI = os.environ["GRAPH_URI"]
-	conn = Neo4jConnection(uri=GRAPH_URI, user="neo4j", pwd="password")
+	cfg = ConfigSet(name=const.APP_CFG, config_file=config_file)
+	spark = SparkConfig().spark_session(config_name=const.APP_CFG, app_name="update-graph")
 
-	spark = SparkConfig().spark_session(config_name=config_file, app_name="update-graph")
+	conn = Neo4jConnection(uri=cfg.get_value(name=const.APP_CFG, jsonpath='GRAPH_URI'),
+		user=cfg.get_value(name=const.APP_CFG, jsonpath='GRAPH_USER'),
+		pwd=cfg.get_value(name=const.APP_CFG, jsonpath='GRAPH_PW'))
 
 	# get project / table path
-	if "BASE_PATH" in os.environ:
-		base_path = os.environ["BASE_PATH"]
+	if "MIND_ROOT_DIR" in os.environ:
+		base_path = os.environ["MIND_ROOT_DIR"]
 	else:
 		base_path = "/gpfs/mskmindhdp_emc/data/"
-	project_dir = os.path.join(base_path, project_name)
+	project_dir = os.path.join(base_path, project)
 	logger.info("Got project path : " + project_dir)
 
 	# load table
-	table_path = os.path.join(project_dir, const.BASE, table_name)
+	table_path = os.path.join(project_dir, const.TABLE_DIR, table)
 	df = spark.read.format("delta").load(table_path)
 
 	# get graph info
-	table_name = table_name.upper()
-	graphs = GraphEnum[table_name].value
+	table = table.upper()
+	graphs = GraphEnum[table].value
 
 	for graph in graphs:
 		src = graph.src
@@ -46,9 +55,12 @@ def update_graph(project_name, table_name, config_file):
 		logger.info("Update graph with {0} - {1} - {2}".format(src, relationship, target))
 
 		def add_to_graph(record: pd.DataFrame) -> pd.DataFrame:
-		    conn.query(f'''MERGE (n:"{src}"{{"{src}": "{record[src_column_name]}"}}) MERGE (m:"{target}"{{"{target}": "{record[target_column_name]}"}}) MERGE (n)-[r:"{relationship}"]->(m)''')
+			conn.query(f'''MERGE (n:"{src}"{{"{src}": "{record[src]}"}}) MERGE (m:"{target}"{{"{target}": "{record[target]}"}}) MERGE (n)-[r:"{relationship}"]->(m)''')
 
-		df = df.groupBy(src_column_name).applyInPandas(add_to_graph, schema=df.schema)
+		# subset dataframe
+		df = df.select(F.col(src_column_name).alias(src), F.col(target_column_name).alias(target))
+
+		df.groupBy(src).applyInPandas(add_to_graph, schema=df.schema)
 
 if __name__ == "__main__":
 	update_graph()
---
+++ tests/data_processing/services/test_graph_service.py
@@ -0,0 +1,53 @@
+import pytest
+from pytest_mock import mocker
+import os, shutil
+from pyspark import SQLContext
+from click.testing import CliRunner
+from neo4j import Record
+
+from data_processing.common.config import ConfigSet
+from data_processing.common.Neo4jConnection import Neo4jConnection
+from data_processing.common import constants as const
+from data_processing.common.sparksession import SparkConfig
+from data_processing.services.graph_service import update_graph
+
+current_dir = os.getcwd()
+project_name = 'test-project'
+
+@pytest.fixture(autouse=True)
+def spark(monkeypatch):
+    print('------setup------')
+    # setup env
+    monkeypatch.setenv("MIND_ROOT_DIR", os.path.join(current_dir, "tests/data_processing/testdata/data"))
+    stream = os.popen('which python')
+    pypath = stream.read().rstrip()
+    monkeypatch.setenv("PYSPARK_PYTHON", pypath)
+
+    ConfigSet(name=const.APP_CFG, config_file='tests/test_config.yaml')
+    spark = SparkConfig().spark_session(config_name=const.APP_CFG, app_name='test-graph-service')
+    yield spark
+
+    print('------teardown------')
+
+
+def test_cli_dicom_table(mocker, spark):
+
+    sqlc = SQLContext(spark)
+
+    # mock data
+    mocker.patch.object(Neo4jConnection, 'query')
+    record1 = {'source': {'value': 'P-123'}, 'sink': {'value': '1.1.1'}, 'path': [{'value': 'P-123'}, 'ID_LINK', {'value': 'RIA_11-111_111'}, 'HAS_SCAN', {'value': '1.1.1'}]}
+    record2 = {'source': {'value': 'P-123'}, 'sink': {'value': '1.2.2'}, 'path': [{'value': 'P-123'}, 'ID_LINK', {'value': 'RIA_11-111_222'}, 'HAS_SCAN', {'value': '1.2.2'}]}
+    r1 = Record(record1)
+    r2 = Record(record2)
+    query_result = [r1, r2]
+    Neo4jConnection.query.return_value = query_result
+
+    runner = CliRunner()
+    result = runner.invoke(update_graph, [
+        '-p', project_name,
+        '-t', 'dicom',
+        '-f', 'tests/test_config.yaml'])
+
+    assert result.exit_code == 0
+
