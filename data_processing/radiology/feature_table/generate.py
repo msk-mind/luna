ea2077e281735525d4c51517128044fa3c570b47
Rose <rosed2@pllimsksparky1.mskcc.org>
2020-12-31 18:11:38 -0500

    embedding pngs works

--- data_processing/radiology/feature_table/generate.py
+++ data_processing/radiology/feature_table/generate.py
@@ -12,6 +12,7 @@ import pandas as pd
 from PIL import Image 
 from medpy.io import load
 from filehash import FileHash
+from io import BytesIO
 
 from data_processing.common.CodeTimer import CodeTimer
 from data_processing.common.config import ConfigSet
@@ -20,15 +21,20 @@ from data_processing.common.custom_logger import init_logger
 import data_processing.common.constants as const
 
 from pyspark.sql import functions as F
-from pyspark.sql.types import StringType, IntegerType
+from pyspark.sql.types import StringType, IntegerType, StructType, StructField, BinaryType
 
 logger = init_logger()
 logger.info("Starting data_processing.radiology.feature_table.generate")
 
 
-def generate_uuid(path):
-    
-    uuid = FileHash('sha256').hash_file(path)
+def generate_uuid(content):
+    content = BytesIO(content)
+
+    import EnsureByteContext
+    with EnsureByteContext.EnsureByteContext():
+        uuid = FileHash('sha256').hash_file(content)
+ 
+    #uuid = FileHash('sha256').hash_file(path)
     return "FEATURE-"+uuid
 
 def crop_and_save(png, features, xmin, ymin, xmax, ymax):
@@ -40,8 +46,8 @@ def crop_and_save(png, features, xmin, ymin, xmax, ymax):
     im = Image.open(png)
     im_crop = im.crop((xmin, ymin, xmax, ymax))
     im_crop.save(feature_path)
-
     return feature_path
+    #return im_crop.tobytes()
 
 
 @click.command()
@@ -120,8 +126,8 @@ def generate_feature_table(cfg):
             """
             # Save feature pngs
             print("Processing accession number: " + str(df.accession_number.values[0]))
-            features = os.path.join(feature_dir, df.accession_number.values[0])
-            os.makedirs(features, exist_ok=True)
+            #features = os.path.join(feature_dir, df.accession_number.values[0])
+            #os.makedirs(features, exist_ok=True)
 
             # mha file path
             file_path = df.path.values[0].split(':')[-1]
@@ -165,12 +171,16 @@ def generate_feature_table(cfg):
             # Crop all overlay, dicom pngs.
             dicom_paths = []
             for png in df.dicom_path.values:
-                feature_path = crop_and_save(png, features, xmin, ymin, xmax, ymax)
+                #feature_path = crop_and_save(png, features, xmin, ymin, xmax, ymax)
+                im = Image.open(png)
+                feature_path = im.crop((xmin, ymin, xmax, ymax)).tobytes()
                 dicom_paths.append(feature_path)
 
             overlay_paths = []
             for png in df.overlay_path.values:
-                feature_path = crop_and_save(png, features, xmin, ymin, xmax, ymax)
+                #feature_path = crop_and_save(png, features, xmin, ymin, xmax, ymax)
+                im = Image.open(png)
+                feature_path = im.crop((xmin, ymin, xmax, ymax)).tobytes()
                 overlay_paths.append(feature_path)
 
             df["dicom_path"] = dicom_paths
@@ -178,11 +188,18 @@ def generate_feature_table(cfg):
             
             return df
         
-
-        df = df.groupBy("accession_number").applyInPandas(crop_images, schema = df.schema)
+        logger.info(df.schema)
+        schema = StructType([StructField("accession_number",StringType(),True),
+                             StructField("instance_number",IntegerType(),True),
+                             StructField("dicom_path",BinaryType(),True),
+                             StructField("overlay_path",BinaryType(),True),
+                             StructField("png_record_uuid",StringType(),True),
+                             StructField("path",StringType(),True)])
+        df = df.groupBy("accession_number").applyInPandas(crop_images, schema = schema)
        
         logger.info("Cropped pngs")
 
+        spark.sparkContext.addPyFile("./data_processing/common/EnsureByteContext.py")
         generate_uuid_udf = F.udf(generate_uuid, StringType())
         df = df.withColumn("feature_record_uuid", F.lit(generate_uuid_udf(df.overlay_path)))
 
