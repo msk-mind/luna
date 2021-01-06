2593a5e171319c990f798e6c1769df80718ec0fe
Rose <rosed2@pllimsksparky1.mskcc.org>
2021-01-06 11:46:44 -0500

    calculate image size from the bytearray

--- data_processing/radiology/feature_table/unpack.py
+++ data_processing/radiology/feature_table/unpack.py
@@ -7,6 +7,7 @@ Data Transfer / Unpack binaries
 """
 import os, time
 import click
+import math
 import pandas as pd
 from PIL import Image 
 from io import BytesIO
@@ -61,7 +62,6 @@ def binary_to_png(cfg):
 
     DESTINATION_PATH = cfg.get_value(path=const.DATA_CFG+"::DESTINATION_PATH")
     COLUMN_NAME = cfg.get_value(path=const.DATA_CFG+"::COLUMN_NAME")
-    IMAGE_SIZE = int(cfg.get_value(path=const.DATA_CFG+"::IMAGE_SIZE"))
 
     # create destination directory
     os.makedirs(DESTINATION_PATH, exist_ok=True)
@@ -69,11 +69,15 @@ def binary_to_png(cfg):
     # unpack COLUMN_NAME
     for index, row in df.iterrows():
         # mode set to L for b/w images, RGB for colored images.
-        mode = "L"
+        array_len = len(row[COLUMN_NAME])
+        if "dicom" == COLUMN_NAME.lower():
+            dim = int(math.sqrt(array_len))
+            mode = "L"
         if "overlay" == COLUMN_NAME.lower():
+            dim = int(math.sqrt(array_len//3))
             mode = "RGB"
 
-        image = Image.frombytes(mode, (IMAGE_SIZE, IMAGE_SIZE), bytes(row[COLUMN_NAME]))
+        image = Image.frombytes(mode, (dim, dim), bytes(row[COLUMN_NAME]))
 
         image_dir = os.path.join(DESTINATION_PATH, COLUMN_NAME, row.accession_number)
         os.makedirs(image_dir, exist_ok=True)
