6ffbf7dabae7bda55485409b99456ec820fee445
Aukerman <aukermaa@pllimsksparky1.mskcc.org>
2021-01-19 13:17:32 -0500

    Tests and associated data

---
+++ tests/data_processing/scanManager/test_generateScan.py
@@ -0,0 +1,43 @@
+import os, shutil
+import pytest
+from pytest_mock import mocker
+from click.testing import CliRunner
+
+from data_processing.scanManager.generateScan import cli as cli_generateScan
+
+cwd = os.getcwd()
+
+def test_cli_mhd(mocker, monkeypatch):
+    monkeypatch.setenv("MIND_GPFS_DIR", cwd+"/tests/data_mock/")
+
+    # mock graph connection
+    mocker.patch("data_processing.scanManager.generateScan.get_container_data", return_value={'SeriesInstanceUID': "1.240.0.1", 'path':f'file:{cwd}/tests/data_processing/testdata/data/2.000000-CTAC-24716/dicoms/'})
+    mocker.patch("data_processing.scanManager.generateScan.get_method_data", return_value={'file_ext':'mhd'})
+
+    mock_db = mocker.patch("data_processing.common.Neo4jConnection.Neo4jConnection.query", return_value='DB')
+
+    runner = CliRunner()
+    result = runner.invoke(cli_generateScan, [
+        '-c', 'test-cohort',
+        '-s', '1',
+        '-m', 'test-method'])
+    assert "mhd-e0c2c6182c51052cffc1c4ae4f0e6c1af9a666998f3a291107c060344cf16f64" in mock_db.call_args_list[0][0][0]
+    assert result.exit_code == 0 
+
+def test_cli_nrrd(mocker, monkeypatch):
+    monkeypatch.setenv("MIND_GPFS_DIR", cwd+"/tests/data_mock/")
+
+    # mock graph connection
+    mocker.patch("data_processing.scanManager.generateScan.get_container_data", return_value={'SeriesInstanceUID': "1.240.0.1", 'path':f'file:{cwd}/tests/data_processing/testdata/data/2.000000-CTAC-24716/dicoms/'})
+    mocker.patch("data_processing.scanManager.generateScan.get_method_data", return_value={'file_ext':'nrrd'})
+
+    mock_db = mocker.patch("data_processing.common.Neo4jConnection.Neo4jConnection.query", return_value='DB')
+
+    runner = CliRunner()
+    result = runner.invoke(cli_generateScan, [
+        '-c', 'test-cohort',
+        '-s', '1',
+        '-m', 'test-method'])
+    assert "nrrd-e0c2c6182c51052cffc1c4ae4f0e6c1af9a666998f3a291107c060344cf16f64" in mock_db.call_args_list[0][0][0]
+    assert result.exit_code == 0 
+
