bb21098ea2cd7e95dede1af2266896bc7cb7e567
doori <doori.rose@gmail.com>
2021-02-03 07:34:29 -0500

    clean up graph code in refined regional annotation

--- tests/data_processing/pathology/common/test_utils.py
+++
@@ -1,6 +0,0 @@
-from data_processing.pathology.common.utils import *
-
-def test_get_add_triple_str():
-    res = get_add_triple_str("123", "geojson_record_uuid", "SVGEOJSON-default-123")
-
-    assert 'MERGE (n:slide{slide_id: "123"}) MERGE (m:geojson_record_uuid{geojson_record_uuid: "SVGEOJSON-default-123"}) MERGE (n)-[r:HAS_RECORD]->(m)' == res
