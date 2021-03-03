1a40a42d234279aa3e6a00f3a296176d1b066c56
doori <doori.rose@gmail.com>
2021-03-03 16:02:08 -0500

    add label in csv for testing

--- tests/data_processing/radiology/proxy_table/annotation/test_generate.py
+++ tests/data_processing/radiology/proxy_table/annotation/test_generate.py
@@ -37,7 +37,8 @@ def test_cli_mha(spark):
     df = spark.read.format("delta").load(os.path.join(os.getcwd(), mha_table_path))
     df.show(10, False)
     assert df.count() == 1
-    assert set(['modificationTime', 'length','scan_annotation_record_uuid', 'path', 'accession_number', 'series_number', 'metadata']) \
+    columns = ['modificationTime', 'length','scan_annotation_record_uuid', 'path', 'accession_number', 'series_number', 'label', 'metadata']
+    assert set(columns) \
             == set(df.columns)
 
     df.unpersist()
@@ -54,8 +55,8 @@ def test_cli_mhd(spark):
     df = spark.read.format("delta").load(os.path.join(os.getcwd(), mhd_table_path))
     df.show(10, False)
     assert df.count() == 1
-    # No metadata csv provided, and no series_number column added
-    assert set(['modificationTime', 'length','scan_annotation_record_uuid', 'path', 'accession_number', 'metadata']) \
+    columns = ['modificationTime', 'length','scan_annotation_record_uuid', 'path', 'accession_number', 'series_number', 'label', 'metadata']
+    assert set(columns) \
             == set(df.columns)
 
     df.unpersist()
