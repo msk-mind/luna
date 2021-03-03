1ed45d4d6e4712e41667174bbd4b36c14bf7b40f
doori <doori.rose@gmail.com>
2021-03-03 09:32:19 -0500

    use rad metadata to populate series number

--- data_processing/radiology/proxy_table/annotation/generate.py
+++ data_processing/radiology/proxy_table/annotation/generate.py
@@ -12,7 +12,7 @@ from data_processing.common.sparksession import SparkConfig
 from data_processing.common.utils import generate_uuid
 import data_processing.common.constants as const
 
-from pyspark.sql.functions import udf, lit, array
+from pyspark.sql.functions import udf, lit, array, col
 from pyspark.sql.types import StringType, MapType
 
 from medpy.io import load
@@ -42,52 +42,51 @@ def parse_metadata(path):
 
 
 @click.command()
-@click.option('-t', '--template_file', default=None, type=click.Path(exists=True),
+@click.option('-d', '--data_config_file', default=None, type=click.Path(exists=True),
               help="path to yaml template file containing information required for scan annotation ingestion."
-                   "See data_ingestion_template.yaml")
-@click.option('-f', '--config_file', default='config.yaml', type=click.Path(exists=True),
+                   "See data_config.yaml.template")
+@click.option('-a', '--app_config_file', default='config.yaml', type=click.Path(exists=True),
               help="path to config file containing application configuration. See config.yaml.template")
 @click.option('-p', '--process_string', default='all',
               help='comma separated list of processes to run or replay: e.g. transfer,delta,graph, or all')
-def cli(template_file, config_file, process_string):
+def cli(data_config_file, app_config_file, process_string):
     """
     This module generates annotation tables for radiology data based on information specified in the template file.
     This supports MHA and MHD as the raw files. Specify mha or mhd as the file_type and data_type in the ingestion template. 
 
     Example:
         python -m data_processing.radiology.proxy_table.annotation.generate \
-        --template_file {PATH_TO_TEMPLATE_FILE} \
-        --config_file {PATH_TO_CONFIG_FILE}
-        --process_string transfer,delta
+        --data_config_file {PATH_TO_DATA_CONFIG_FILE} \
+        --app_config_file {PATH_TO_APP_CONFIG_FILE}
+        --process_string delta
 
     """
     with CodeTimer(logger, 'generate proxy table'):
         processes = process_string.lower().strip().split(",")
-        logger.info('data_ingestions_template: ' + template_file)
-        logger.info('config_file: ' + config_file)
+        logger.info('data_ingestions_template: ' + data_config_file)
+        logger.info('config_file: ' + app_config_file)
         logger.info('processes: ' + str(processes))
 
         # load configs
-        cfg = ConfigSet(name=const.APP_CFG, config_file=config_file)
+        cfg = ConfigSet(name=const.APP_CFG, config_file=app_config_file)
 
         # TODO add transfer logic when we establish a standard method for scan annotations.
 
         # subprocess - create proxy table
         if 'delta' in processes or 'all' in processes:
-            exit_code = create_proxy_table(template_file)
+            exit_code = create_proxy_table(data_config_file)
             if exit_code != 0:
                 logger.error("Delta table creation had errors. Exiting.")
                 return
 
-        # TODO call graph service?
 
-def create_proxy_table(template_file):
+def create_proxy_table(data_config):
 
     exit_code = 0
-    cfg = ConfigSet(name=const.DATA_CFG, config_file=template_file, schema_file=const.SCHEMA_FILE)
+    cfg = ConfigSet(name=const.DATA_CFG, config_file=data_config)
 
     spark = SparkConfig().spark_session(config_name=const.APP_CFG, 
-        app_name="data_processing.radiology.proxy_table.generate_annotations")
+        app_name="data_processing.radiology.proxy_table.annotation.generate")
 
     logger.info("generating proxy table... ")
 
@@ -107,8 +106,18 @@ def create_proxy_table(template_file):
             .load(cfg.get_value(path=const.DATA_CFG+'::RAW_DATA_PATH')) \
             .drop("content")
 
-        df = df.withColumn("accession_number", parse_accession_number_udf(df.path)) \
-            .withColumn("scan_annotation_record_uuid", generate_uuid_udf(df.path, array([lit("SCAN_ANNOTATION")]))) \
+        df = df.withColumn("accession_number", parse_accession_number_udf(df.path))
+
+        # TODO: once radiology segmentation information is on REDCap join with that table.
+        # for now, we join with a curated csv that includes AccessionNumber, SeriesNumber columns
+        annotation_metadata = spark.read.load(cfg.get_value(path=const.DATA_CFG+'::METADATA_CSV'),
+                                              format="csv", header="true", inferSchema="true")
+        annotation_metadata = annotation_metadata.select(col("AccessionNumber").alias("accession_number"),
+                                                         col("SeriesNumber").alias("series_number"))
+
+        df = df.join(annotation_metadata, on="accession_number", how="left")
+
+        df = df.withColumn("scan_annotation_record_uuid", generate_uuid_udf(df.path, array([lit("SCAN_ANNOTATION")]))) \
             .withColumn("metadata", parse_metadata_udf(df.path))
 
     # parse all dicoms and save
