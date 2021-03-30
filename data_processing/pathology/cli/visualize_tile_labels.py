255130f75881f0d54c851e31ecd5840966c99caa
Patel <pateld6@pllimsksparky1.mskcc.org>
2021-03-30 15:47:13 -0400

    add regional_annotation labels from annotation ETL as score to tile df. updated visualization code for this as well

--- data_processing/pathology/cli/visualize_tile_labels.py
+++ data_processing/pathology/cli/visualize_tile_labels.py
@@ -48,7 +48,7 @@ def visualize_tile_labels_with_container(cohort_id: str, container_id: str, meth
     # Do some setup
     container   = Container( cfg ).setNamespace(cohort_id).lookupAndAttach(container_id)
     method_id   = method_data.get("job_tag", "none")
-
+    
     image_node  = container.get("WholeSlideImage", method_data['input_wsi_tag']) 
     label_node  = container.get("TileScores", method_data['input_label_tag']) 
 
