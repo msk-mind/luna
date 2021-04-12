33a5c3658e89e8b911e8cbf4683404c90f84c177
doori <doori.rose@gmail.com>
2021-04-12 10:24:57 -0400

    clean up returns and example json

--- data_processing/pathology/cli/dsa/dsa_api_handler.py
+++ data_processing/pathology/cli/dsa/dsa_api_handler.py
@@ -5,12 +5,12 @@ import time
 
 image_id_regex = "(.*).svs"
 
-'''
-Returns the DSA item uuid from the provided image_name
-Expects image_name to be the image name with svs extension. (e.g. "HobI20-681330526186.svs" or "TCGA-GM-A2DB-01Z-00-DX1.9EE36AA6-2594-44C7-B05C-91A0AEC7E511.svs" are valid entries)
-'''
-def get_item_uuid(image_name, uri, token):
 
+def get_item_uuid(image_name, uri, token):
+    """
+    Returns the DSA item uuid from the provided image_name
+    Expects image_name to be the image name with svs extension.
+    """
     image_id = re.search(image_id_regex, image_name).group(1)
 
     get_dsa_uuid_url = f"http://{uri}/api/v1/item?text={image_id}&limit=50&sort=lowerName&sortdir=1"
@@ -27,11 +27,11 @@ def get_item_uuid(image_name, uri, token):
                     return dsa_uuid
     return None
 
-'''
-Pushes DSA annotation to DSA, adding given item_uuid (slide-specific id)
-'''
-def push_annotation_to_dsa_image(item_uuid, dsa_annotation_json, uri, token):
 
+def push_annotation_to_dsa_image(item_uuid, dsa_annotation_json, uri, token):
+    """
+    Pushes DSA annotation to DSA, adding given item_uuid (slide-specific id)
+    """
     start = time.time()
 
     headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Girder-Token': f'{token}'}
@@ -45,8 +45,10 @@ def push_annotation_to_dsa_image(item_uuid, dsa_annotation_json, uri, token):
         print("Annotation successfully pushed to DSA.")
         print("Time to push annotation", time.time() - start)
         print(f"http://{uri}/histomics#?image={item_uuid}")
+        return 0
     else:
         print("Error in annotation upload:", response.status_code, response.reason)
+        return 1
 
 
 
@@ -64,6 +66,8 @@ def system_check(uri, token):
 
     if response.status_code == 200:
         print("Successfully connected to DSA.")
+        return 0
     else:
         print(f"Please check your host/token.")
+        return 1
 
