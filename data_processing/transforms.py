29ce0c0901d07aa5fa7958c9b33724c343441014
Aukerman <aukermaa@pllimsksparky1.mskcc.org>
2021-04-26 15:17:09 -0400

    Load pipeline from yaml

---
+++ data_processing/transforms.py
@@ -0,0 +1,4 @@
+from data_processing.radiology.cli.generate_scan        import generate_scan_with_container as generate_scan
+from data_processing.radiology.cli.randomize_contours   import randomize_contours_with_container as randomize_contours
+from data_processing.radiology.cli.extract_radiomics    import extract_radiomics_with_container as extract_radiomics
+from data_processing.radiology.cli.collect_csv_segment  import collect_result_segment_with_container as collect_results
