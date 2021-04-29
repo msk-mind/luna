8e59eb7ea8632efe7e2a208b5dfad9bd9c006054
Patel <pateld6@pllimsksparky3.mskcc.org>
2021-04-29 16:02:36 -0400

    separate upload into new cli, write dsa annots to disk

--- data_processing/pathology/cli/dsa/dsa_api_handler.py
+++ data_processing/pathology/cli/dsa/dsa_api_handler.py
@@ -6,26 +6,48 @@ import time
 image_id_regex = "(.*).svs"
 
 
-def get_item_uuid(image_name, uri, token):
+def get_collection_uuid(uri, token, collection_name):
+    get_collection_id_url = f"http://{uri}/api/v1/collection?text={collection_name}&limit=5&sort=name&sortdir=1"
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Girder-Token': f'{token}'}
+    get_collection_id_url_response = requests.get(get_collection_id_url, headers=headers)
+    get_collection_id_response = json.loads(get_collection_id_url_response.text)
+    
+    collection_id_dicts = get_collection_id_response
+    
+    for collection_id_dict in collection_id_dicts:
+        print("collection_id_dict", collection_id_dict)
+        if collection_id_dict['name'] == collection_name:
+            collection_id = collection_id_dict['_id']
+            print(f"Collection {collection_name} found with id: {collection_id}")
+            return collection_id
+        
+    print(f"Collection {collection_name} not found")
+    return None
+    
+
+def get_item_uuid(image_name, uri, token, collection_name):
     """
     Returns the DSA item uuid from the provided image_name
     Expects image_name to be the image name with svs extension.
     """
+
+    collection_uuid = get_collection_uuid(uri, token, collection_name)
     image_id = re.search(image_id_regex, image_name).group(1)
 
     get_dsa_uuid_url = f"http://{uri}/api/v1/item?text={image_id}&limit=50&sort=lowerName&sortdir=1"
     headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Girder-Token': f'{token}'}
     get_dsa_uuid_response = requests.get(get_dsa_uuid_url, headers=headers)
-    uuid_response = json.loads(get_dsa_uuid_response.text) 
+    uuid_response = json.loads(get_dsa_uuid_response.text)
     
     if len(uuid_response) > 0 :
         # multiple entries can come up based on substring matches, return the correct item id by checking name field in dictionary.
         for uuid_response_dict in uuid_response:
             if "name" in uuid_response_dict and "_id" in uuid_response_dict:
-                if uuid_response_dict["name"] == image_name:
+                if uuid_response_dict["name"] == image_name and uuid_response_dict['baseParentId'] == collection_uuid:
                     dsa_uuid =  uuid_response_dict['_id']
                     return dsa_uuid
     return None
+
 
 
 def push_annotation_to_dsa_image(item_uuid, dsa_annotation_json, uri, token):
