9372c3c8c06b87dd0138cc3f9219dba7ab1c5a98
doori <doori.rose@gmail.com>
2021-07-13 14:26:10 -0400

    update to datastore v2

--- tests/data_processing/pathology/cli/test_infer_tile_labels.py
+++ tests/data_processing/pathology/cli/test_infer_tile_labels.py
@@ -1,28 +1,21 @@
 from click.testing import CliRunner
+import pytest
 
-from data_processing.common.Neo4jConnection import Neo4jConnection
 from data_processing.pathology.cli.infer_tile_labels import cli
 
 
-def test_cli(mocker):
-
-    mocker.patch.object(Neo4jConnection, 'query')
-    props = {"id(container)":"id",
-             "container.name":"name",
-             "container.qualified_address":"address",
-             "container.type":"slide",
-             "labels(container)":"tag",
-             "data":{"type":"WholeSlideImage", "name":"123"}}
-    Neo4jConnection.query.return_value = [props]
-
-    mocker.patch.object(Neo4jConnection, 'test_connection')
+def test_cli():
 
     runner = CliRunner()
+
     result = runner.invoke(cli, [
-        '-a', 'tests/data_processing/pathology/cli/testdata/test_config.yaml',
-        '-c', 'test',
-        '-s', '123',
-        '-m', 'tests/data_processing/pathology/cli/testdata/infer_tile_labels_resnet18.json'])
+            '-a', 'tests/data_processing/pathology/cli/testdata/test_config.yaml',
+            '-s', '123',
+            '-m', 'tests/data_processing/pathology/cli/testdata/infer_tile_labels_resnet18.json'])
 
     # No longer error gracefully -- can update tests with proper data and they'll work
     assert result.exit_code == 1
+    # no model in the testdata
+    assert type(result.exception) == FileNotFoundError
+
+
