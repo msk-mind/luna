cc3742cb7cba56f7e6a930c1aec55728a7481628
doori <doori.rose@gmail.com>
2021-10-18 11:01:52 -0400

    fix: push to pypi then release

--- pyluna-core/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for luna"""
-
-__author__ = """msk-mind"""
-__email__ = 'CompOncBST@mskcc.org'
-__version__ = '0.0.1'
--- pyluna-radiology/__init__.py
+++
@@ -1,3 +0,0 @@
-__author__ = """msk-mind"""
-__email__ = 'CompOncBST@mskcc.org'
-__version__ = '0.0.1'
