7621a20d515b620f336fc9e7e23cb3b56717ace0
doori <doori.rose@gmail.com>
2021-10-18 13:31:26 -0400

    fix: update version pattern

---
+++ pyluna-radiology/__init__.py
@@ -0,0 +1,7 @@
+from pkg_resources import DistributionNotFound, get_distribution
+
+try:
+    __version__ = get_distribution("pyluna").version
+except DistributionNotFound:
+    # package is not installed
+    pass
