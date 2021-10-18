1657bb45b58af49f15331fdfd9b94c8e5a89ee71
doori <doori.rose@gmail.com>
2021-10-18 14:28:44 -0400

    fix: update init version

--- pyluna-common/__init__.py
+++ pyluna-common/__init__.py
@@ -1,7 +1,2 @@
-from pkg_resources import DistributionNotFound, get_distribution
-
-try:
-    __version__ = get_distribution("pyluna").version
-except DistributionNotFound:
-    # package is not installed
-    pass
+with open('../VERSION.txt') as fp:
+    __version__ = fp.readlines()[0]
