53c5038996aac38c27d0e7520f97b7368d191c31
Aukerman <aukermaa@pllimsksparky3.mskcc.org>
2021-12-10 17:47:28 -0500

    Update tile inference code

--- pyluna-pathology/luna/pathology/cli/infer_tile_labels.py
+++ pyluna-pathology/luna/pathology/cli/infer_tile_labels.py
@@ -1,95 +1,92 @@
 # General imports
 import os, json, logging
 import click
-import yaml
 
-# From common
 from luna.common.custom_logger   import init_logger
-from luna.common.DataStore       import DataStore_v2
-from luna.common.config          import ConfigSet
 
-# From radiology.common
-from luna.pathology.common.preprocess import run_model
+init_logger()
+logger = logging.getLogger('infer_tille_labels')
+
+import torch
+from torch.utils.data import DataLoader
+from luna.pathology.common.ml import BaseTorchTileDataset, BaseTorchTileClassifier
+
+import pandas as pd
+from tqdm import tqdm
+
+
+class TileDatasetGithub(BaseTorchTileDataset):
+    def setup(self, repo_name, transform_name):
+        self.transform = torch.hub.load(repo_name, transform_name)
+    def preprocess(self, input_tile):
+        return self.transform(input_tile)
+    
+class TileClassifierGithub(BaseTorchTileClassifier):
+    def setup(self, repo_name, model_name, weight_tag):
+        self.model = torch.hub.load(repo_name, model_name, weight_tag=weight_tag)
+    def predict(self, input_tiles):
+        return self.model(input_tiles)
 
 @click.command()
-@click.option('-a', '--app_config', required=True,
-              help="application configuration yaml file. See config.yaml.template for details.")
-@click.option('-s', '--datastore_id', required=True,
-              help='datastore name. usually a slide id.')
-@click.option('-m', '--method_param_path', required=True,
-              help='json file with method parameters for loading a saved model.')
-def cli(app_config, datastore_id, method_param_path):
-    """Infer tile labels with a trained model.
+@click.option('-i', '--input_data', required=True,
+              help='path to input data')
+@click.option('-o', '--output_dir', required=True,
+              help='path to output directory to save results')
+@click.option('-r', '--repo_name', required=True,
+              help="repository name to pull model and weight from, e.g. msk-mind/luna-ml")
+@click.option('-t', '--transform_name', required=True,
+              help="torch hub transform name")   
+@click.option('-m', '--model_name', required=True,
+              help="torch hub model name")    
+@click.option('-w', '--weight_tag', required=True,
+              help="weight tag filename")    
 
-    app_config - application configuration yaml file. See config.yaml.template for details.
-
-    datastore_id - datastore name. usually a slide id.
-
-    method_param_path - json file with method parameters for loading a saved model.
-
-    - input_label_tag: job tag used to generate tile labels
-
-    - job_tag: job tag for the inference
-
-    - model_package: package to load your model e.g. luna.pathology.models.ov_tissuenet
-
-    - model: model details e.g. {
-          "checkpoint_path": "/path/to/checkpoint",
-          "n_classes": 4
-      }
-
-    - root_path: path to output directory
+def cli(input_data, output_dir, repo_name, transform_name, model_name, weight_tag):
     """
-    init_logger()
-
-    with open(method_param_path, 'r') as yaml_file:
-        method_data = yaml.safe_load(yaml_file)
-    infer_tile_labels_with_datastore(app_config, datastore_id, method_data)
-
-def infer_tile_labels_with_datastore(app_config: str, datastore_id: str, method_data: dict):
-    """Infer tile labels with a trained model.
-
-    Args:
-        app_config (string): path to application configuration file.
-        datastore_id (string): datastore name. usually a slide id.
-        method_data (dict): method parameters including input, output details.
-
-    Returns:
-        None
+    infer_tile_labels
+        -i /gpfs/mskmindhdp_emc/data/CRC_21-167/tables/tiles/1412934/crc_generate_all_tiles/TileImages/data/ 
+        -o ./tmp/1412934 -r msk-mind/luna-ml:main 
+        -t tissue_tile_net_transform 
+        -m tissue_tile_net_model_5_class 
+        -w main:tissue_net_2021-01-19_21.05.24-e17.pth
     """
-    logger = logging.getLogger(f"[datastore={datastore_id}]")
+    logger.info(f"Running infer_tille_labels, input_data={input_data}, output_dir={output_dir}")
+    os.makedirs(output_dir, exist_ok=True)
 
-    # Do some setup
-    cfg = ConfigSet("APP_CFG",  config_file=app_config)
-    datastore   = DataStore_v2(method_data.get("root_path"))
-    method_id   = method_data.get("job_tag", "none")
+    logger.info(f"Loading model and transform:, repo_name={repo_name}, transform_name={transform_name}, model_name={model_name}")
+    logger.info(f"Using weights weight_tag={weight_tag}")
+    tile_dataset     = TileDatasetGithub(tile_path=input_data, repo_name=repo_name, transform_name=transform_name)
+    tile_classifier  = TileClassifierGithub(repo_name=repo_name, model_name=model_name, weight_tag=weight_tag)
 
-    tile_path           = datastore.get(datastore_id, method_data['input_label_tag'], "TileImages")
-    if tile_path is None:
-        raise ValueError("Tile path not found")
+    tile_loader = DataLoader(
+            tile_dataset,
+            num_workers=24,
+            batch_size=256,
+            pin_memory=True
+        )
 
-    tile_image_path     = os.path.join(tile_path, "tiles.slice.pil")
-    tile_label_path     = os.path.join(tile_path, "address.slice.csv")
+    with torch.no_grad():
+        df_scores = pd.concat([tile_classifier(index, data) for index, data in tqdm(tile_loader)])
+        
+    if hasattr(tile_classifier.model, 'class_labels'):
+        logger.info(f"Mapping column labels -> {tile_classifier.model.class_labels}")
+        df_scores = df_scores.rename(columns=tile_classifier.model.class_labels)
 
-    # get image_id
-    # TODO - allow -s to take in slide (container) id
+    df_output = tile_dataset.tile_manifest.join(df_scores)    
 
-    try:
-        # Data just goes under namespace/name
-        # TODO: This path is really not great, but works for now
-        output_dir = os.path.join(method_data.get("root_path"), datastore_id, method_id, "TileScores", "data")
-        if not os.path.exists(output_dir): os.makedirs(output_dir)
+    logger.info(df_output)
 
-        properties = run_model(tile_image_path, tile_label_path, output_dir, method_data)
+    output_file = os.path.join(output_dir, "tile_scores_and_labels_pytorch_inference.csv")
+    df_output.to_csv(output_file)
 
-    except Exception as e:
-        logger.exception (f"{e}, stopping job execution...")
-        raise e
+    properties = {
+        "data": output_file,
+        "total_tiles": len(df_output),
+        "available_labels": list(df_output.columns)
+    }
 
-    # Save metadata
     with open(os.path.join(output_dir, "metadata.json"), "w") as fp:
         json.dump(properties, fp)
-
 
 if __name__ == "__main__":
     cli()
--- pyluna-pathology/tests/luna/pathology/cli/test_infer_tile_labels.py
+++ pyluna-pathology/tests/luna/pathology/cli/test_infer_tile_labels.py
@@ -8,9 +8,13 @@ def test_cli():
     runner = CliRunner()
 
     result = runner.invoke(cli, [
-            '-a', 'pyluna-pathology/tests/luna/pathology/cli/testdata/test_config.yml',
-            '-s', '123',
-            '-m', 'pyluna-pathology/tests/luna/pathology/cli/testdata/infer_tile_labels_resnet18.yml'])
+            '-i', 'pyluna-pathology/tests/luna/pathology/cli/testdata/123/data',
+            '-o', './tmp/123',
+            '-r', 'msk-mind/luna-ml',
+            '-t', 'tissue_tile_net_transform',
+            '-m', 'tissue_tile_net_model_5_class',
+            '-w', 'main:tissue_net_2021-01-19_21.05.24-e17.pth',
+            ])
 
     # No longer error gracefully -- can update tests with proper data and they'll work
     assert result.exit_code == 1
