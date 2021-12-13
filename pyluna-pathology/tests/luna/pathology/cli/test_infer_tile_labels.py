38eff97dbcd1e1fa717b7b3997271d23d7595fa7
Aukerman <aukermaa@pllimsksparky3.mskcc.org>
2021-12-13 13:40:45 -0500

    Fix test args

--- pyluna-pathology/tests/luna/pathology/cli/test_infer_tile_labels.py
+++ pyluna-pathology/tests/luna/pathology/cli/test_infer_tile_labels.py
@@ -10,10 +10,10 @@ def test_cli():
     result = runner.invoke(cli, [
             '-i', 'pyluna-pathology/tests/luna/pathology/cli/testdata/123/data',
             '-o', './tmp/123',
-            '-r', 'msk-mind/luna-ml',
-            '-t', 'tissue_tile_net_transform',
-            '-m', 'tissue_tile_net_model_5_class',
-            '-w', 'main:tissue_net_2021-01-19_21.05.24-e17.pth',
+            '-rn', 'msk-mind/luna-ml',
+            '-tn', 'tissue_tile_net_transform',
+            '-mn', 'tissue_tile_net_model_5_class',
+            '-wt', 'main:tissue_net_2021-01-19_21.05.24-e17.pth',
             ])
 
     # No longer error gracefully -- can update tests with proper data and they'll work
