c3e976ecae3134b208719637770305e73f3af3a4
Aukerman <aukermaa@pllimsksparky3.mskcc.org>
2021-12-13 13:29:08 -0500

    Better params handling

--- pyluna-common/tests/luna/common/test_utils.py
+++ pyluna-common/tests/luna/common/test_utils.py
@@ -1,5 +1,6 @@
 from luna.common.utils import *
 import sys
+import pytest
 
 def test_generate_uuid():
     uuid = generate_uuid("file:./pyluna-common/tests/luna/common/test_config.yml", ["FEATURE", "label"])
@@ -10,3 +11,68 @@ def test_get_absolute_path():
     absolute_path= get_absolute_path(__file__, '../data_ingestion_template_invalid.yml')
     assert absolute_path.startswith('/')
     assert absolute_path.endswith('pyluna-common/tests/luna/data_ingestion_template_invalid.yml')
+
+
+
+def test_validate_params__simple():
+    params_list = [('input',str), ('threshold', float)]
+    
+    given_params = {
+        'input':'data.json',
+        'threshold':0.5,
+        'extra':1,
+    }
+    out_params = validate_params(given_params, params_list)
+
+    assert out_params == {
+        'input':'data.json',
+        'threshold':0.5,
+    }
+
+def test_validate_params__casting():
+    params_list = [('input',str), ('threshold', float)]
+    
+    given_params = {
+        'input':'data.json',
+        'threshold':'0.5', # Cast string
+        'extra':1,
+    }
+    out_params = validate_params(given_params, params_list)
+
+    assert out_params == {
+        'input':'data.json',
+        'threshold':0.5,
+    }
+
+def test_validate_params__missing_value():
+    with pytest.raises(RuntimeError):
+
+        params_list = [('input',str), ('threshold', float)]
+        
+        given_params = {
+            'threshold':0.5, # Cast string
+            'extra':1,
+        }
+        out_params = validate_params(given_params, params_list)
+
+        assert out_params == {
+            'input':'data.json',
+            'threshold':0.5,
+        }
+
+def test_validate_params__wrong_value():
+    with pytest.raises(RuntimeError):
+
+        params_list = [('input',str), ('threshold', float)]
+        
+        given_params = {
+            'input':'data.json',
+            'threshold':'Five', # Cast string
+            'extra':1,
+        }
+        out_params = validate_params(given_params, params_list)
+
+        assert out_params == {
+            'input':'data.json',
+            'threshold':0.5,
+        }
--- pyluna-pathology/luna/pathology/cli/infer_tile_labels.py
+++ pyluna-pathology/luna/pathology/cli/infer_tile_labels.py
@@ -1,11 +1,13 @@
 # General imports
-import os, json, logging
+import os, json, logging, yaml
 import click
 
 from luna.common.custom_logger   import init_logger
 
 init_logger()
 logger = logging.getLogger('infer_tille_labels')
+
+from luna.common.utils import validate_params
 
 import torch
 from torch.utils.data import DataLoader
@@ -14,7 +16,64 @@ from luna.pathology.common.ml import BaseTorchTileDataset, BaseTorchTileClassifi
 import pandas as pd
 from tqdm import tqdm
 
+_params = [('input_data', str), ('output_dir', str), ('repo_name', str), ('transform_name', str), ('model_name', str), ('weight_tag', str), ('num_cores', int), ('batch_size', int)]
 
+@click.command()
+@click.option('-i', '--input_data', required=False,
+              help='path to input data')
+@click.option('-o', '--output_dir', required=False,
+              help='path to output directory to save results')
+@click.option('-rn', '--repo_name', required=False,
+              help="repository name to pull model and weight from, e.g. msk-mind/luna-ml")
+@click.option('-tn', '--transform_name', required=False,
+              help="torch hub transform name")   
+@click.option('-mn', '--model_name', required=False,
+              help="torch hub model name")    
+@click.option('-wt', '--weight_tag', required=False,
+              help="weight tag filename")  
+@click.option('-nc', '--num_cores', required=False,
+              help="Number of cores to use", default=4)  
+@click.option('-bx', '--batch_size', required=False,
+              help="weight tag filename", default=256)    
+@click.option('-m', '--method_param_path', required=False,
+              help='json file with method parameters for tile generation and filtering')
+def cli(**cli_kwargs):
+    """
+    Run with explicit arguments:
+        infer_tile_labels
+            -i 1412934/data/TileImages 
+            -o 1412934/data/TilePredictions
+            -r msk-mind/luna-ml:main 
+            -t tissue_tile_net_transform 
+            -m tissue_tile_net_model_5_class 
+            -w main:tissue_net_2021-01-19_21.05.24-e17.pth
+
+    Run with implicit arguments
+        infer_tile_labels -m 1412934/data/TilePredictions/metadata.json
+    
+    Run with mixed arguments
+        infer_tile_labels --input_data 1412934/data/TileImages -m 1412934/data/TilePredictions/metadata.json
+    """
+    kwargs = {}
+
+    # Get params from param file
+    if cli_kwargs.get('method_param_path'):
+        with open(cli_kwargs.get('method_param_path'), 'r') as yaml_file:
+            yaml_kwargs = yaml.safe_load(yaml_file)
+        kwargs.update(yaml_kwargs) # Fill from json
+    
+    for key in list(cli_kwargs.keys()):
+        if cli_kwargs[key] is None: del cli_kwargs[key]
+
+    # Override with CLI arguments
+    kwargs.update(cli_kwargs) # 
+
+    # Validate them
+    kwargs = validate_params(kwargs, _params)
+
+    infer_tile_labels(**kwargs)
+
+# We are acting a bit like a consumer of the base classes here-
 class TileDatasetGithub(BaseTorchTileDataset):
     def setup(self, repo_name, transform_name):
         self.transform = torch.hub.load(repo_name, transform_name)
@@ -27,44 +86,33 @@ class TileClassifierGithub(BaseTorchTileClassifier):
     def predict(self, input_tiles):
         return self.model(input_tiles)
 
-@click.command()
-@click.option('-i', '--input_data', required=True,
-              help='path to input data')
-@click.option('-o', '--output_dir', required=True,
-              help='path to output directory to save results')
-@click.option('-r', '--repo_name', required=True,
-              help="repository name to pull model and weight from, e.g. msk-mind/luna-ml")
-@click.option('-t', '--transform_name', required=True,
-              help="torch hub transform name")   
-@click.option('-m', '--model_name', required=True,
-              help="torch hub model name")    
-@click.option('-w', '--weight_tag', required=True,
-              help="weight tag filename")    
+def infer_tile_labels(input_data, output_dir, repo_name, transform_name, model_name, weight_tag, num_cores, batch_size):
+    """Generate tile addresses, scores and optionally annotation labels using models stored in torch.hub format
 
-def cli(input_data, output_dir, repo_name, transform_name, model_name, weight_tag):
+    Args:
+        input_data (str): path to application configuration file.
+        output_dir (str): datastore name. usually a slide id.
+        repo_name (str): method parameters including input, output details.
+        transform_name (str):
+        model_name (str):
+        weight_tag (str):
+
+    Returns:
+        None
     """
-    infer_tile_labels
-        -i /gpfs/mskmindhdp_emc/data/CRC_21-167/tables/tiles/1412934/crc_generate_all_tiles/TileImages/data/ 
-        -o ./tmp/1412934 -r msk-mind/luna-ml:main 
-        -t tissue_tile_net_transform 
-        -m tissue_tile_net_model_5_class 
-        -w main:tissue_net_2021-01-19_21.05.24-e17.pth
-    """
-    logger.info(f"Running infer_tille_labels, input_data={input_data}, output_dir={output_dir}")
+    input_params = validate_params(locals(), _params) # Capture input parameters as dict
     os.makedirs(output_dir, exist_ok=True)
 
-    logger.info(f"Loading model and transform:, repo_name={repo_name}, transform_name={transform_name}, model_name={model_name}")
+    # Get our model and transforms and construct the Tile Dataset and Classifier
+    logger.info(f"Loading model and transform: repo_name={repo_name}, transform_name={transform_name}, model_name={model_name}")
     logger.info(f"Using weights weight_tag={weight_tag}")
+
     tile_dataset     = TileDatasetGithub(tile_path=input_data, repo_name=repo_name, transform_name=transform_name)
     tile_classifier  = TileClassifierGithub(repo_name=repo_name, model_name=model_name, weight_tag=weight_tag)
 
-    tile_loader = DataLoader(
-            tile_dataset,
-            num_workers=24,
-            batch_size=256,
-            pin_memory=True
-        )
+    tile_loader = DataLoader(tile_dataset, num_workers=num_cores, batch_size=batch_size, pin_memory=True)
 
+    # Generate aggregate dataframe
     with torch.no_grad():
         df_scores = pd.concat([tile_classifier(index, data) for index, data in tqdm(tile_loader)])
         
@@ -79,14 +127,16 @@ def cli(input_data, output_dir, repo_name, transform_name, model_name, weight_ta
     output_file = os.path.join(output_dir, "tile_scores_and_labels_pytorch_inference.csv")
     df_output.to_csv(output_file)
 
-    properties = {
-        "data": output_file,
+    # Save our properties and params
+    extra_props = {
         "total_tiles": len(df_output),
         "available_labels": list(df_output.columns)
     }
 
+    input_params.update(extra_props)
+
     with open(os.path.join(output_dir, "metadata.json"), "w") as fp:
-        json.dump(properties, fp)
+        json.dump(input_params, fp, indent=4, sort_keys=True)
 
 if __name__ == "__main__":
     cli()
