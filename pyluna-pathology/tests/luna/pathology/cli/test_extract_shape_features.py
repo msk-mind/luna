973337d931e1520386efe153281b4b0891d0d489
armaank <kohli@cooper.edu>
2022-02-16 11:49:28 -0500

    test: adding test for feature extractor

---
+++ pyluna-pathology/tests/luna/pathology/cli/test_extract_shape_features.py
@@ -0,0 +1,29 @@
+import os
+from click.testing import CliRunner
+
+import pandas as pd
+from luna.pathology.cli.extract_shape_features import cli
+import tifffile 
+import numpy as np
+
+from luna.pathology.schemas import SlideTiles
+
+def test_cli_generate_mask(tmp_path):
+    runner = CliRunner()
+    result = runner.invoke(cli, [
+        'pyluna-pathology/tests/luna/pathology/cli/testdata/data/generate_tile_mask//tile_mask.tif',
+        '-o', tmp_path,
+        '-lc', 'Background,Tumor',
+    ])
+
+    assert result.exit_code == 0
+
+    assert os.path.exists(f"{tmp_path}/shape_features.csv")
+    assert os.path.exists(f"{tmp_path}/metadata.yml")
+    df = pd.read_csv(f"{tmp_path}/shape_features.csv")
+
+    assert len(df) == 2
+
+
+
+
