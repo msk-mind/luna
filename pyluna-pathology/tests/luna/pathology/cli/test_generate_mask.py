f2b26c6e3baf65ed2461072440da435736348ed5
doori <doori.rose@gmail.com>
2022-03-08 08:53:10 -0500

    tests: generate mask, stain texture

---
+++ pyluna-pathology/tests/luna/pathology/cli/test_generate_mask.py
@@ -0,0 +1,29 @@
+import os
+from click.testing import CliRunner
+
+from luna.pathology.cli.generate_mask import cli
+import openslide
+
+path = "pyluna-pathology/tests/luna/pathology/cli/testdata/data/generate_mask"
+
+
+def test_cli(tmp_path):
+    runner = CliRunner()
+    result = runner.invoke(
+        cli,
+        [
+            "pyluna-pathology/tests/luna/pathology/cli/testdata/data/123.svs",
+            "pyluna-pathology/tests/luna/pathology/testdata/data/test-project/pathology_annotations/123456_annotation_from_halo.xml",
+            "-o",
+            tmp_path,
+            "-an",
+            "Tumor",
+        ],
+    )
+
+    assert result.exit_code == 0
+
+    assert os.path.exists(f"{tmp_path}/mask_full_res.tif")
+    assert os.path.exists(f"{tmp_path}/metadata.yml")
+
+    openslide.ImageSlide(f"{tmp_path}/mask_full_res.tif")
