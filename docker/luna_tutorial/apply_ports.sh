8ef5bb47d5e235fe6e3a0d75d13b785c211081d8
arfathpasha <arfathpasha@gmail.com>
2022-05-16 09:14:35 -0400

    feat: 1217 add dremio to tutorial (#318)
    * dynamically apply minio ports
    
    * bug fix
    
    * add vol mnt to minio
    
    * add dremio to tutorial
    
    * provision dremio
    
    * make all root creds uniform
    
    * minor fixes
    
    * lock image versions
    
    * add s3 source
    
    * replace underscore in project name
    
    * minor fixes
    
    * replace underscore in project name
    
    * add docs

--- docker/luna_tutorial/apply_ports.sh
+++ docker/luna_tutorial/apply_ports.sh
@@ -5,7 +5,7 @@
 
 # find and apply open port for girder
 echo "Finding available port for girder..."
-PORT=$(./find_open_port.sh 8080 65535)
+PORT=$(./find_open_port.sh 8080 8887)
 echo "Found port $PORT"
 rm -f .dsa_port
 echo $PORT >> .dsa_port
@@ -28,7 +28,7 @@ fi
 
 # find and apply open port for jupyterlab
 echo "Finding available ports for jupyterlab..."
-PORT=$(./find_open_port.sh 8888 65535)
+PORT=$(./find_open_port.sh 8888 8999)
 echo "Found port $PORT"
 rm -f .jupyter_port
 echo $PORT >> .jupyter_port
@@ -43,7 +43,7 @@ else   # for linux
 fi
 
 
-PORT=$(./find_open_port.sh 6006 65535)
+PORT=$(./find_open_port.sh 6006 8079)
 echo "Found port $PORT"
 
 unameOut="$(uname -s)"
@@ -53,4 +53,78 @@ then   # for mac
     sed -i '' -E "s/[0-9]{4}:6006/$PORT:6006/g" docker-compose.yml
 else   # for linux
     sed -i -r "s/[0-9]{4}:6006/$PORT:6006/g" docker-compose.yml
+fi
+
+# find and apply open ports for minio
+echo "Finding available ports for minio api url..."
+PORT=$(./find_open_port.sh 9000 9046)
+echo "Found port $PORT"
+echo $PORT >> .minio_api_port
+
+unameOut="$(uname -s)"
+
+if [[ "$unameOut" == *"Darwin"* ]];
+then   # for mac
+    sed -i '' -E "s/[0-9]{4}:9000/$PORT:9000/g" docker-compose.yml
+else   # for linux
+    sed -i -r "s/[0-9]{4}:9000/$PORT:9000/g" docker-compose.yml
+fi
+
+
+echo "Finding available ports for minio web url..."
+PORT=$(./find_open_port.sh 9500 9999)
+echo "Found port $PORT"
+rm -f .minio_port
+echo $PORT >> .minio_web_port
+
+unameOut="$(uname -s)"
+
+if [[ "$unameOut" == *"Darwin"* ]];
+then   # for mac
+    sed -i '' -E "s/[0-9]{4}:9001/$PORT:9001/g" docker-compose.yml
+    sed -i '' -E "s/minio:[0-9]{4}/minio:$PORT/g" docker-compose.yml
+else   # for linux
+    sed -i -r "s/[0-9]{4}:9001/$PORT:9001/g" docker-compose.yml
+    sed -i -r "s/minio:[0-9]{4}/minio:$PORT/g" docker-compose.yml
+fi
+
+
+# find and apply open ports for dremio
+echo "Finding available ports for dremio..."
+PORT=$(./find_open_port.sh 9047 9499)
+echo "Found port $PORT"
+rm -f .dremio_port
+echo $PORT >> .dremio_port
+
+unameOut="$(uname -s)"
+
+if [[ "$unameOut" == *"Darwin"* ]];
+then   # for mac
+    sed -i '' -E "s/[0-9]{4}:9047/$PORT:9047/g" docker-compose.yml
+else   # for linux
+    sed -i -r "s/[0-9]{4}:9047/$PORT:9047/g" docker-compose.yml
+fi
+
+PORT=$(./find_open_port.sh 31010 31110)
+echo "Found port $PORT"
+
+unameOut="$(uname -s)"
+
+if [[ "$unameOut" == *"Darwin"* ]];
+then   # for mac
+    sed -i '' -E "s/[0-9]{5}:31010/$PORT:31010/g" docker-compose.yml
+else   # for linux
+    sed -i -r "s/[0-9]{5}:31010/$PORT:31010/g" docker-compose.yml
+fi
+
+PORT=$(./find_open_port.sh 45678 45778)
+echo "Found port $PORT"
+
+unameOut="$(uname -s)"
+
+if [[ "$unameOut" == *"Darwin"* ]];
+then   # for mac
+    sed -i '' -E "s/[0-9]{5}:45678/$PORT:45678/g" docker-compose.yml
+else   # for linux
+    sed -i -r "s/[0-9]{5}:45678/$PORT:45678/g" docker-compose.yml
 fi
---
+++ docker/luna_tutorial/vmount/provision_dremio.py
@@ -0,0 +1,167 @@
+'''
+Created on May 12, 2022
+
+@author: pashaa@mskcc.org
+'''
+
+
+import requests
+import json
+import argparse
+import sys
+import logging
+import socket
+
+logger = logging.getLogger(__name__)
+# See http://docs.python.org/3.3/howto/logging.html#configuring-logging-for-a-library
+logging.getLogger(__name__).addHandler(logging.StreamHandler)
+
+
+def login(url, username, password):
+  ''':param username
+     :param password
+  '''
+  endpoint = url+"/apiv2/login"
+
+  payload = json.dumps({
+    "userName": username,
+    "password": password,
+  })
+  headers = {
+    'Content-Type': 'application/json'
+  }
+
+  response = requests.request("POST", endpoint, headers=headers, data=payload)
+
+  return response
+
+
+def create_nas_source(url, token):
+  '''
+  Create a datalake NAS source.
+
+  :param token: authorization token
+  :return:
+  '''
+  endpoint = url+"/api/v3/source"
+
+  payload = json.dumps({
+    "_type": "source",
+    "name": "local_lake",
+    "description": "tutorial data lake",
+    "type": "NAS",
+    "config": {
+      "path": "/data"
+    },
+    "metadataPolicy": {
+      "authTTLMs": 60000,
+      "datasetRefreshAfterMs": 60000,
+      "datasetExpireAfterMs": 60000,
+      "namesRefreshMs": 60000,
+      "datasetUpdateMode": "PREFETCH"
+    },
+    "accelerationRefreshPeriodMs": 60000,
+    "accelerationGracePeriodMs": 60000
+  })
+  headers = {
+    'Authorization': 'Bearer '+token,
+    'Content-Type': 'application/json'
+  }
+
+  response = requests.request("POST", endpoint, headers=headers, data=payload)
+
+  return response
+
+
+def create_s3_source(dremio_url, minio_url, token):
+  '''
+  Create a datalake S3 source.
+
+  :param url: URL of S3 source
+  :param token: authorization token
+  :return:
+  '''
+  print('Creating S3 source to {}...'.format(minio_url))
+
+  endpoint = dremio_url+"/api/v3/source"
+
+  payload = json.dumps({
+    "_type": "source",
+    "name": "local_s3_lake",
+    "description": "tutorial data lake",
+    "type": "S3",
+    "config": {
+      "accessKey": "admin",
+      "accessSecret": "password1",
+      "secure": "false",
+      "externalBucketList": [],
+      "rootPath": "/",
+      "compatibilityMode": "true",
+      "propertyList": [
+         {"name": "fs.s3a.path.style.access", "value": "true"},
+         {"name": "fs.s3a.endpoint",          "value": minio_url}
+      ]
+    },
+    "metadataPolicy": {
+      "authTTLMs": 60000,
+      "datasetRefreshAfterMs": 60000,
+      "datasetExpireAfterMs": 60000,
+      "namesRefreshMs": 60000,
+      "datasetUpdateMode": "PREFETCH"
+    },
+    "accelerationRefreshPeriodMs": 60000,
+    "accelerationGracePeriodMs": 60000
+  })
+  headers = {
+    'Authorization': 'Bearer '+token,
+    'Content-Type': 'application/json'
+  }
+
+  response = requests.request("POST", endpoint, headers=headers, data=payload)
+
+  return response
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Provision a Dremio instance')
+    parser.add_argument(
+        '--username', '-u', default=None,  help='admin username')
+    parser.add_argument(
+        '--password', '-p', default=None, help='admin password')
+    parser.add_argument(
+        '--dremio_port', '-d', default=None, help='dremio api port')
+    parser.add_argument(
+        '--minio_port', '-m', default=None, help='minio api port')
+    parser.add_argument(
+      '--verbose', '-v', default=0, help='Increase verbosity')
+
+    print('\nProvisioning Dremio...')
+
+    opts = parser.parse_args(args=sys.argv[1:])
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    logger.setLevel(max(1, logging.WARNING - 10 * opts.verbose))
+
+    dremio_url = "http://dremio:"+opts.dremio_port
+    minio_url = socket.gethostbyname('minio')+':'+opts.minio_port  # only works with ip address
+
+
+    response = login(dremio_url, opts.username, opts.password)
+
+    print('Logged in to Dremio...')
+
+    if response.status_code == 200:
+        token = response.json()['token']
+        
+        response = create_s3_source(dremio_url, minio_url, token)
+        
+        if response.status_code != 200:
+            print('Dremio provision error: {}'.format(response.json()['errorMessage']))
+        else:
+            print('Successful!\n')
+            
+    else:
+        print('Dremio provision error: unable to log into dremio '
+              'using username={} password={}'.format(opts.username, opts.password))
+
+
+
