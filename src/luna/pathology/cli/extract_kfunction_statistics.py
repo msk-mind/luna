a861f9dd9b7ab2dac1238b411a2efcb1e5b28da5
Tom Pollard <pollardw@mskcc.org>
2023-08-04 11:07:05 -0400

    Added spatial analysis tutorial notebook
    This is just Armaan's original notebook, updated for the current
    state of luna.  It still uses the CLIs.

--- src/luna/pathology/cli/extract_kfunction_statistics.py
+++ src/luna/pathology/cli/extract_kfunction_statistics.py
@@ -9,7 +9,7 @@ from dask.distributed import progress
 from loguru import logger
 from tqdm.contrib.itertools import product
 
-from luna.common.dask import get_or_create_dask_client
+from luna.common.dask import get_or_create_dask_client, configure_dask_client
 from luna.common.utils import get_config, save_metadata, timed
 from luna.pathology.common.utils import coord_to_address
 from luna.pathology.spatial.stats import Kfunction
@@ -43,6 +43,9 @@ def cli(
         pd.DataFrame: metadata about function call
     """
     config = get_config(vars())
+
+    configure_dask_client()
+
     df_stats = extract_kfunction(
         config["input_cell_objects_urlpath"],
         config["tile_size"],
